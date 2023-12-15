cb07cfcba8c31dc92e628a3ea0d2a7c8065d9954
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-12-16 07:02:37 +0900

    윈도우 이벤트를 처리할 빈 프로시저 추가

--- ProjectA/Source/Application.cpp
+++ ProjectA/Source/Application.cpp
@@ -1,7 +1,12 @@
 #include <cstdint>
 #include <windows.h>
 
-int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_  int32_t nCmdShow)
+LRESULT CALLBACK WindowProc(HWND windowHandle, uint32_t message, WPARAM wParam, LPARAM lParam)
+{
+	return DefWindowProc(windowHandle, message, wParam, lParam);
+}
+
+int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
 	return 0;
 }
