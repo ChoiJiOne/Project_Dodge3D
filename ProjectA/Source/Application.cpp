2b3520bab1a40a787357e3feeda3673d560a9c99
CHOIJIONE\choij <choijione02@gmail.com>
2023-12-16 18:17:46 +0900

    기존의 윈도우 생성 코드를 Window class로 대체

--- ProjectA/Source/Application.cpp
+++ ProjectA/Source/Application.cpp
@@ -1,8 +1,11 @@
 #include <cstdint>
+#include <string>
 #include <windows.h>
 
-HINSTANCE GInstance = nullptr;
-HWND GWindowHandle = nullptr;
+#include "Window.h"
+#include "WindowsAssertion.h"
+
+Window window;
 
 LRESULT CALLBACK WindowProc(HWND windowHandle, uint32_t message, WPARAM wParam, LPARAM lParam)
 {
@@ -12,7 +15,7 @@ LRESULT CALLBACK WindowProc(HWND windowHandle, uint32_t message, WPARAM wParam, 
 		break;
 
 	case WM_CLOSE:
-		DestroyWindow(windowHandle);
+		window.Destroy();
 		break;
 
 	case WM_DESTROY:
@@ -25,51 +28,10 @@ LRESULT CALLBACK WindowProc(HWND windowHandle, uint32_t message, WPARAM wParam, 
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
-	GInstance = hInstance;
+	Window::RegisterWindowClass(L"ProjectA", WindowProc);
 
-	WNDCLASSEXW wcexw = {};
-	wcexw.cbSize = sizeof(WNDCLASSEXW);
-	wcexw.style = CS_HREDRAW | CS_VREDRAW;
-	wcexw.lpfnWndProc = WindowProc;
-	wcexw.cbClsExtra = 0;
-	wcexw.cbWndExtra = 0;
-	wcexw.hInstance = GInstance;
-	wcexw.hIcon = LoadIcon(GInstance, IDI_APPLICATION);
-	wcexw.hIconSm = LoadIcon(GInstance, IDI_APPLICATION);
-	wcexw.hCursor = LoadCursor(GInstance, IDC_ARROW);
-	wcexw.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcexw.lpszMenuName = nullptr;
-	wcexw.lpszClassName = L"ProjectA";
-
-	if (!RegisterClassExW(&wcexw))
-	{
-		return -1;
-	}
-
-	RECT windowRect = {0, 0, 800, 600};
-	AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
-
-	GWindowHandle = CreateWindowExW(
-		0,
-		L"ProjectA",
-		L"ProjectA",
-		WS_OVERLAPPEDWINDOW, // WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX 크기 변경 불가
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		windowRect.right - windowRect.left,
-		windowRect.bottom - windowRect.top,
-		nullptr,
-		nullptr,
-		GInstance,
-		nullptr
-	);
-
-	if (!GWindowHandle)
-	{
-		return -1;
-	}
-
-	ShowWindow(GWindowHandle, SW_SHOW);
+	window.Create(L"ProjectA", 200, 200, 800, 600, true, true, true);
+	ShowWindow(window.GetHandle(), SW_SHOW);
 
 	MSG msg = {};
 	while (msg.message != WM_QUIT)
@@ -80,8 +42,7 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 			DispatchMessageW(&msg);
 		}
 	}
-
-	BOOL check = UnregisterClassW(L"ProjectA", GInstance);
 	
+	Window::UnregisterWindowClass();
 	return 0;
 }
