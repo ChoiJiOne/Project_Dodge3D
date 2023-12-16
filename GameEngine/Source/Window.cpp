598ac90bec30598a9c8c82d49b1ac6dbb6619993
CHOIJIONE\choij <choijione02@gmail.com>
2023-12-16 16:50:52 +0900

    윈도우 클래스 등록/해제 부분에 Windows 어선셜 매크로 추가

--- GameEngine/Source/Window.cpp
+++ GameEngine/Source/Window.cpp
@@ -1,5 +1,7 @@
 #include "Window.h"
 
+#include "WindowsAssertion.h"
+
 bool Window::bIsRegisterWindowClass_ = false;
 HINSTANCE Window::hInstance_ = nullptr;
 std::wstring Window::windowClassName_;
@@ -10,41 +12,36 @@ Window::~Window()
 
 void Window::RegisterWindowClass(const std::wstring& windowClassName, WINDOWPROC windowProc)
 {
-	if (!bIsRegisterWindowClass_)
-	{
-		hInstance_ = GetModuleHandleW(nullptr);
-		windowClassName_ = windowClassName;
+	ASSERT(!bIsRegisterWindowClass_, "already register window class...");
+	ASSERT(windowProc != nullptr, "Window procedure is nullptr...");
 
-		WNDCLASSEXW wcexw = {};
-		wcexw.cbSize = sizeof(WNDCLASSEXW);
-		wcexw.style = CS_HREDRAW | CS_VREDRAW;
-		wcexw.lpfnWndProc = windowProc;
-		wcexw.cbClsExtra = 0;
-		wcexw.cbWndExtra = 0;
-		wcexw.hInstance = hInstance_;
-		wcexw.hIcon = LoadIcon(hInstance_, IDI_APPLICATION);
-		wcexw.hIconSm = LoadIcon(hInstance_, IDI_APPLICATION);
-		wcexw.hCursor = LoadCursor(hInstance_, IDC_ARROW);
-		wcexw.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-		wcexw.lpszMenuName = nullptr;
-		wcexw.lpszClassName = windowClassName_.c_str();
+	hInstance_ = GetModuleHandleW(nullptr);
+	windowClassName_ = windowClassName;
 
-		if (!RegisterClassExW(&wcexw))
-		{
-			return;
-		}
+	WNDCLASSEXW wcexw = {};
+	wcexw.cbSize = sizeof(WNDCLASSEXW);
+	wcexw.style = CS_HREDRAW | CS_VREDRAW;
+	wcexw.lpfnWndProc = windowProc;
+	wcexw.cbClsExtra = 0;
+	wcexw.cbWndExtra = 0;
+	wcexw.hInstance = hInstance_;
+	wcexw.hIcon = LoadIcon(hInstance_, IDI_APPLICATION);
+	wcexw.hIconSm = LoadIcon(hInstance_, IDI_APPLICATION);
+	wcexw.hCursor = LoadCursor(hInstance_, IDC_ARROW);
+	wcexw.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcexw.lpszMenuName = nullptr;
+	wcexw.lpszClassName = windowClassName_.c_str();
 
-		bIsRegisterWindowClass_ = true;
-	}
+	WINDOWS_ASSERT(RegisterClassExW(&wcexw) != 0, "failed to register window class...");
+	bIsRegisterWindowClass_ = true;
 }
 
 void Window::UnregisterWindowClass()
 {
-	if (bIsRegisterWindowClass_)
-	{
-		UnregisterClassW(windowClassName_.c_str(), hInstance_);
+	ASSERT(bIsRegisterWindowClass_, "haven't registered before or have already unregistered...");
 
-		hInstance_ = nullptr;
-		bIsRegisterWindowClass_ = false;
-	}
+	UnregisterClassW(windowClassName_.c_str(), hInstance_);
+
+	hInstance_ = nullptr;
+	bIsRegisterWindowClass_ = false;
 }
