bcb1be683f80caa5cdc8a486514305f8342b5b2e
CHOIJIONE\choij <choijione02@gmail.com>
2023-12-16 16:42:29 +0900

    평가식을 검사하는 어선셜 매크로 추가

--- GameEngine/Include/Assertion.h
+++ GameEngine/Include/Assertion.h
@@ -56,3 +56,48 @@ inline void AssertPrintF(const wchar_t* format, ...)
 	OutputDebugStringW(buffer);
 }
 #endif
+
+
+/**
+ * @brief 평가식을 검사합니다.
+ * 
+ * @param Expression 검사할 호출값입니다.
+ * @param ... 평가식을 만족하지 못할 경우 표시할 가변 인자 메시지입니다.
+ *
+ * @note
+ * - Debug 모드와 Release 모드에서는 평가식을 검사하지만 Shipping 모드에서는 평가식을 검사하지 않습니다.
+ * - 디버거가 존재하면 브레이크 포인트가 걸립니다.
+ * - 디버거가 존재하지 않으면 크래시 덤프 파일을 생성합니다.
+ */
+#if defined(DEBUG)
+#ifndef ASSERT
+#define ASSERT(Expression, ...)\
+{\
+	if(!(bool)(Expression))\
+	{\
+		AssertPrintF("\nAssertion check point failed!\nFile : %s\nLine : %d\nExpression : %s\nMessage : ", __FILE__, __LINE__, #Expression);\
+		AssertPrintF(__VA_ARGS__);\
+		AssertPrintF("\n");\
+		__debugbreak();\
+		ExitProcess(-1);\
+	}\
+}
+#endif
+#elif defined(RELEASE)
+#ifndef ASSERT
+#define ASSERT(Expression, ...)\
+{\
+	if(!(bool)(Expression))\
+	{\
+		AssertPrintF("\nAssertion check point failed!\nFile : %s\nLine : %d\nExpression : %s\nMessage : ", __FILE__, __LINE__, #Expression);\
+		AssertPrintF(__VA_ARGS__);\
+		AssertPrintF("\n");\
+		__debugbreak();\
+	}\
+}
+#endif
+#else // defined(SHIPPING)
+#ifndef ASSERT
+#define ASSERT(Expression, ...) ((void)(Expression))
+#endif
+#endif
