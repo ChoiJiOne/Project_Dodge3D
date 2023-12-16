544a280fd9bde740b65e898f5f9e6dd502cc8385
CHOIJIONE\choij <choijione02@gmail.com>
2023-12-16 18:16:19 +0900

    윈도우 생성/파괴 기능 추가
    - 풀 스크린 모드 구현 완료
    - 윈도우 생성 시 설정한 옵션에 대한 Getter 추가
    - 윈도우 핸들에 대한 Getter 추가

--- GameEngine/Include/Window.h
+++ GameEngine/Include/Window.h
@@ -38,6 +38,53 @@ public:
 
 
 	/**
+	 * @brief 윈도우를 생성합니다.
+	 * 
+	 * @note 풀 스크린 모드 설정 시 일부 옵션은 무시됩니다.
+	 * 
+	 * @param title 윈도우의 타이틀입니다.
+	 * @param x 윈도우 위치의 X좌표입니다. 
+	 * @param y 윈도우 위치의 Y좌표입니다. 
+	 * @param w 윈도우의 가로 크기입니다.
+	 * @param h 윈도우의 세로 크기입니다.
+	 * @param bIsResizable 윈도우의 크기 변환 여부입니다.
+	 * @param bIsShow 윈도우를 표시할 시 여부입니다
+	 * @param bIsFullscreenMode 윈도우를 풀 스크린 모드로 설정할 지 여부입니다.
+	 */
+	void Create(const std::wstring& title, int32_t x, int32_t y, int32_t w, int32_t h, bool bIsResizable, bool bIsShow, bool bIsFullscreenMode);
+
+
+	/**
+	 * @brief 윈도우를 파괴합니다.
+	 */
+	void Destroy();
+
+
+	/**
+	 * @brief 윈도우가 풀 스크린모드인지 확인합니다.
+	 * 
+	 * @return 윈도우가 풀 스크린모드라면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	bool IsFullscreenMode() const { return bIsFullscreenMode_; }
+
+
+	/**
+	 * @brief 윈도우의 크기를 변경할 수 있는지 확인합니다.
+	 * 
+	 * @return 윈도우의 크기를 변경할 수 있다면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	bool IsResizable() const { return bIsResizable_; }
+
+
+	/**
+	 * @brief 윈도우 핸들을 얻습니다.
+	 * 
+	 * @return 윈도우 핸들을 반환합니다.
+	 */
+	HWND GetHandle() { return windowHandle_; }
+
+
+	/**
 	 * @brief 윈도우 클래스를 등록합니다.
 	 * 
 	 * @note 이 정적 메서드는 한 번만 호출할 수 있습니다.
@@ -60,7 +107,7 @@ public:
 	 * @return 윈도우 클래스를 등록했다면 true, 그렇지 않으면 false를 반환합니다.
 	 */
 	static bool IsRegisterWindowClass() { return bIsRegisterWindowClass_; }
-
+	
 
 private:
 	/**
@@ -79,37 +126,7 @@ private:
 	 * @brief 풀 스크린 모드인지 확인합니다.
 	 */
 	bool bIsFullscreenMode_ = false;
-
-
-	/**
-	 * @brief 윈도우를 숨기는지 확인합니다.
-	 */
-	bool bIsHide_ = false;
-
-
-	/**
-	 * @brief 윈도우 상단의 X좌표입니다.
-	 */
-	int32_t positionX_ = 0;
-
-
-	/**
-	 * @brief 윈도우 상단의 Y좌표입니다.
-	 */
-	int32_t positionY_ = 0;
-
-
-	/**
-	 * @brief 윈도우의 가로 크기입니다.
-	 */
-	int32_t width_ = 0;
-
-
-	/**
-	 * @brief 윈도우의 세로 크기입니다.
-	 */
-	int32_t height_ = 0;
-
+	
 
 	/**
 	 * @brief 윈도우 클래스를 등록했는지 확인합니다.
--- GameEngine/Source/Window.cpp
+++ GameEngine/Source/Window.cpp
@@ -8,6 +8,85 @@ std::wstring Window::windowClassName_;
 
 Window::~Window()
 {
+	Destroy();
+}
+
+void Window::Create(const std::wstring& title, int32_t x, int32_t y, int32_t w, int32_t h, bool bIsResizable, bool bIsShow, bool bIsFullscreenMode)
+{
+	ASSERT(windowHandle_ == nullptr, "already create window...");
+	
+	int32_t windowPosX = 0;
+	int32_t windowPosY = 0;
+	int32_t windowWidth = 0;
+	int32_t windowHeight = 0;
+	uint32_t windowStyle = 0;
+
+	if (bIsFullscreenMode)
+	{
+		bIsResizable_ = false;
+		bIsFullscreenMode_ = true;
+
+		windowPosX = 0;
+		windowPosY = 0;
+		windowWidth = static_cast<int32_t>(GetSystemMetrics(SM_CXSCREEN));
+		windowHeight = static_cast<int32_t>(GetSystemMetrics(SM_CYSCREEN));
+		windowStyle = WS_POPUP;
+
+		DEVMODEW devMode = {};
+		devMode.dmSize = sizeof(DEVMODEW);
+		devMode.dmPelsWidth = static_cast<uint32_t>(windowWidth);
+		devMode.dmPelsHeight = static_cast<uint32_t>(windowHeight);
+		devMode.dmBitsPerPel = 32;
+		devMode.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+
+		WINDOWS_ASSERT(ChangeDisplaySettingsW(&devMode, CDS_FULLSCREEN) == DISP_CHANGE_SUCCESSFUL, "failed to set fullscreen mode...");
+	}
+	else
+	{
+		bIsResizable_ = bIsResizable;
+		bIsFullscreenMode_ = false;
+
+		RECT windowRect = { 0, 0, w, h };
+		WINDOWS_ASSERT(AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE), "failed to calculate window size...");
+
+		windowPosX = x;
+		windowPosY = y;
+		windowWidth = windowRect.right - windowRect.left;
+		windowHeight = windowRect.bottom - windowRect.top;
+		windowStyle = bIsResizable ? WS_OVERLAPPEDWINDOW : (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX);
+	}
+
+	windowHandle_ = CreateWindowExW(
+		WS_EX_APPWINDOW,
+		windowClassName_.c_str(),
+		title.c_str(),
+		static_cast<DWORD>(windowStyle),
+		windowPosX,
+		windowPosY,
+		windowWidth,
+		windowHeight,
+		nullptr,
+		nullptr,
+		hInstance_,
+		nullptr
+	);
+	WINDOWS_ASSERT(windowHandle_ != nullptr, "failed to create window...");
+}
+
+void Window::Destroy()
+{
+	if (windowHandle_ == nullptr)
+	{
+		return;
+	}
+
+	if (bIsFullscreenMode_)
+	{
+		WINDOWS_ASSERT(ChangeDisplaySettingsW(nullptr, 0) == DISP_CHANGE_SUCCESSFUL, "failed to set default mode...");
+	}
+
+	WINDOWS_ASSERT(DestroyWindow(windowHandle_), "failed to destroy window...");
+	windowHandle_ = nullptr;
 }
 
 void Window::RegisterWindowClass(const std::wstring& windowClassName, WINDOWPROC windowProc)
