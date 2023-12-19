f8f6ef5a57e9f486dfa07e9d8a1e2d21b1d12d15
CHOIJIONE\choij <choijione02@gmail.com>
2023-12-19 16:47:30 +0900

    기존의 WinMain에 있는 OpenGL 초기화 코드 삭제
    - RenderManager에서 수행하므로 삭제

--- ProjectA/Source/Application.cpp
+++ ProjectA/Source/Application.cpp
@@ -2,11 +2,8 @@
 #include <string>
 #include <windows.h>
 
-#include <glad/glad_wgl.h>
-#include <glad/glad.h>
-
+#include "RenderManager.h"
 #include "Window.h"
-#include "WindowsAssertion.h"
 
 Window window;
 
@@ -18,6 +15,7 @@ LRESULT CALLBACK WindowProc(HWND windowHandle, uint32_t message, WPARAM wParam, 
 		break;
 
 	case WM_CLOSE:
+		RenderManager::Get().Shutdown();
 		window.Destroy();
 		break;
 
@@ -34,66 +32,27 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	Window::RegisterWindowClass(L"ProjectA", WindowProc);
 
 	window.Create(L"ProjectA", 200, 200, 800, 600, true, false);
+	RenderManager::Get().SetRenderTargetWindow(&window);
 
-	HDC hdc = GetDC(window.GetHandle());
+	RenderManager::Get().Startup();
 
-	PIXELFORMATDESCRIPTOR pfd = {};
-	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
-	pfd.nVersion = 1;
-	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_GENERIC_ACCELERATED | PFD_DOUBLEBUFFER | PFD_SWAP_EXCHANGE;
-	pfd.iPixelType = PFD_TYPE_RGBA;
-	pfd.cColorBits = 32;
-	pfd.cAlphaBits = 8;
-	pfd.cDepthBits = 24;
-	pfd.cStencilBits = 8;
-	pfd.iLayerType = PFD_MAIN_PLANE;
-
-	int32_t pixelFormat = ChoosePixelFormat(hdc, &pfd);
-	SetPixelFormat(hdc, pixelFormat, &pfd);
-
-	HGLRC dummyRenderContext = wglCreateContext(hdc);
-	wglMakeCurrent(hdc, dummyRenderContext);
-
-	gladLoadWGLLoader((GLADloadproc)(wglGetProcAddress), hdc);
-
-	const int32_t attributes[] = {
-		WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
-		WGL_CONTEXT_MINOR_VERSION_ARB, 6,
-		WGL_CONTEXT_FLAGS_ARB, 0,
-		WGL_CONTEXT_PROFILE_MASK_ARB,
-		WGL_CONTEXT_CORE_PROFILE_BIT_ARB, 
-		0,
-	};
-
-	HGLRC renderContext = wglCreateContextAttribsARB(hdc, nullptr, attributes);
-	wglMakeCurrent(nullptr, nullptr);
-	wglDeleteContext(dummyRenderContext);
-	wglMakeCurrent(hdc, renderContext);
-
-	gladLoadGL();
-	
-	bool bIsDone = false;
-	while (!bIsDone)
+	while (true)
 	{
 		MSG msg = {};
 		while (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
 		{
-			if (msg.message == WM_QUIT)
-			{
-				bIsDone = true;
-			}
-
 			TranslateMessage(&msg);
 			DispatchMessageW(&msg);
 		}
 
-		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		if (msg.message == WM_QUIT)
+		{
+			break;
+		}
 
-		SwapBuffers(hdc);
+		RenderManager::Get().BeginFrame(1.0f, 0.0f, 0.0f, 1.0f);
+		RenderManager::Get().EndFrame();
 	}
-
-	ReleaseDC(window.GetHandle(), hdc);
 
 	Window::UnregisterWindowClass();
 	return 0;
