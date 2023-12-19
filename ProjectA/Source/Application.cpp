3ea4656f10f95330e6f5fe5f9f7e6dc564081d63
CHOIJIONE\choij <choijione02@gmail.com>
2023-12-19 13:56:35 +0900

    WinMain에 OpenGL 초기화 기능 추가

--- ProjectA/Source/Application.cpp
+++ ProjectA/Source/Application.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <windows.h>
 
+#include <glad/glad_wgl.h>
+#include <glad/glad.h>
+
 #include "Window.h"
 #include "WindowsAssertion.h"
 
@@ -32,16 +35,66 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 
 	window.Create(L"ProjectA", 200, 200, 800, 600, true, false);
 
-	MSG msg = {};
-	while (msg.message != WM_QUIT)
+	HDC hdc = GetDC(window.GetHandle());
+
+	PIXELFORMATDESCRIPTOR pfd = {};
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_GENERIC_ACCELERATED | PFD_DOUBLEBUFFER | PFD_SWAP_EXCHANGE;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cAlphaBits = 8;
+	pfd.cDepthBits = 24;
+	pfd.cStencilBits = 8;
+	pfd.iLayerType = PFD_MAIN_PLANE;
+
+	int32_t pixelFormat = ChoosePixelFormat(hdc, &pfd);
+	SetPixelFormat(hdc, pixelFormat, &pfd);
+
+	HGLRC dummyRenderContext = wglCreateContext(hdc);
+	wglMakeCurrent(hdc, dummyRenderContext);
+
+	gladLoadWGLLoader((GLADloadproc)(wglGetProcAddress), hdc);
+
+	const int32_t attributes[] = {
+		WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
+		WGL_CONTEXT_MINOR_VERSION_ARB, 6,
+		WGL_CONTEXT_FLAGS_ARB, 0,
+		WGL_CONTEXT_PROFILE_MASK_ARB,
+		WGL_CONTEXT_CORE_PROFILE_BIT_ARB, 
+		0,
+	};
+
+	HGLRC renderContext = wglCreateContextAttribsARB(hdc, nullptr, attributes);
+	wglMakeCurrent(nullptr, nullptr);
+	wglDeleteContext(dummyRenderContext);
+	wglMakeCurrent(hdc, renderContext);
+
+	gladLoadGL();
+	
+	bool bIsDone = false;
+	while (!bIsDone)
 	{
-		if (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
+		MSG msg = {};
+		while (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
 		{
+			if (msg.message == WM_QUIT)
+			{
+				bIsDone = true;
+			}
+
 			TranslateMessage(&msg);
 			DispatchMessageW(&msg);
 		}
+
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		SwapBuffers(hdc);
 	}
-	
+
+	ReleaseDC(window.GetHandle(), hdc);
+
 	Window::UnregisterWindowClass();
 	return 0;
 }
