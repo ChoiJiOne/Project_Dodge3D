5c88752f1c2ac0e4a02e87ba3569cf3dbef72bef
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-12-21 08:48:20 +0900

    엔진의 프로젝트 생성 스크립트를 CMake에 맞게 수정
    - Visual Studio를 실행할 지 확인하는 옵션 추가

--- GameEngine/Script/GenerateProjectFiles.bat
+++ GameEngine/Script/GenerateProjectFiles.bat
@@ -1,39 +1,70 @@
 @echo off
 
-@REM 게임 엔진 경로입니다.
-SET engine_path=%~dp0..\
+echo Start "GenerateProjectFiles.bat" script...
 
-@REM Premake5 실행 파일 경로입니다.
-SET premake5_path=%engine_path%Bin\premake5.exe
+@REM CMake가 설치되었는지 확인합니다.
+@echo off
+where cmake > nul 2>&1
+if %ERRORLEVEL% EQU 0 (
+    echo CMake is installed on this system...
+) else (
+    echo CMake is not installed on this system...
+    GOTO:EOF
+)
 
 @REM Visual Studio 버전입니다. 
-SET visualstudio=%1
+SET vs=%1
+
+@REM visual studio 버전을 검사합니다. 2019 혹은 2022입니다.
+if "%vs%" == "vs2019" (
+    SET visualstudio="Visual Studio 16 2019"
+    echo Visual Studio version is "%vs%"...
+) else if "%vs%" == "vs2022" (
+    SET visualstudio="Visual Studio 17 2022"
+    echo Visual Studio version is "%vs%"...
+) else (
+    echo Visual Studio version "%vs%" is illegal...
+    GOTO:EOF
+)
 
 @REM 프로젝트 이름입니다.
 SET project=%2
 
-echo START GenerateProjectFiles.bat...
-
-@REM visual studio 버전을 검사합니다. 2019 혹은 2022입니다.
-if "%visualstudio%" == "vs2019" (
-    echo Visual Studio version is %visualstudio%...
-) else if "%visualstudio%" == "vs2022" (
-    echo Visual Studio version is %visualstudio%...
-) else (
-    echo Visual Studio version %visualstudio% is illegal...
-    GOTO:EOF
-)
-
 @REM 프로젝트 이름의 유효성을 검사합니다.
-if "%project%" == "" (
-    echo project name is empty...
+if %project% == "" (
+    echo Project name is empty...
     GOTO:EOF
 ) else (
-    echo Project name is %project%
+    echo Project name is "%project%"...
 )
 
-echo START premake5.exe...
-%premake5_path% %visualstudio% %project%
+@REM 프로젝트 생성 후 Visual Studio를 실행할 지 여부입니다.
+SET run=%3
+if "%run%" == "on" (
+    SET run="on"
+) else if "%run%" == "On" (
+    SET run="on"
+) else if "%run%" == "off" (
+    SET run="off"
+) else if "%run%" == "Off" (
+    SET run="off"
+) else (
+    SET run="off"
+)
+echo Switch %run% run Visual Studio Solution...
 
-echo START visual studio solution...
-start Solution\%project%.sln
+if not exist %~dp0..\..\Solution (
+    mkdir %~dp0..\..\Solution
+)
+
+pushd %~dp0
+pushd %~dp0..\..\Solution
+
+cmake .. -G %visualstudio% -A "x64"
+
+if %run% == "on" (
+    start %project%.sln
+)
+
+popd
+popd
