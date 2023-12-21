8cf7fc82e8a2ef34fcbe921fc7431858c4fb4df8
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-12-21 09:49:51 +0900

    Visual Studio 솔루션이 존재하지 않으면 솔루션을 생성하는 기능 삭제
    - 대신 솔루션이 감지되지 않으면 메시지를 띄우고 강제로 종료하도록 구현

--- GameEngine/Script/BuildSolution.bat
+++ GameEngine/Script/BuildSolution.bat
@@ -12,37 +12,14 @@ if %ERRORLEVEL% EQU 0 (
     GOTO:EOF
 )
 
-@REM Visual Studio 버전입니다. 
-SET vs=%1
-
-@REM visual studio 버전을 검사합니다. 2019 혹은 2022입니다.
-if "%vs%" == "vs2019" (
-    SET visualstudio="Visual Studio 16 2019"
-    echo Visual Studio version is "%vs%"...
-) else if "%vs%" == "vs2022" (
-    SET visualstudio="Visual Studio 17 2022"
-    echo Visual Studio version is "%vs%"...
-) else (
-    echo Visual Studio version "%vs%" is illegal...
+@REM 프로젝트가 존재하지 않으면 프로젝트를 생성합니다.
+if not exist %~dp0..\..\Solution (
+    echo Can't find Visual Studio Solution...
     GOTO:EOF
 )
 
-@REM 프로젝트가 존재하지 않으면 프로젝트를 생성합니다.
-if not exist %~dp0..\..\Solution (
-    echo Start generate Visual Studio Solution...
-    mkdir %~dp0..\..\Solution
-
-    pushd %~dp0
-    pushd %~dp0..\..\Solution
-
-    cmake .. -G %visualstudio% -A "x64"
-
-    popd
-    popd
-)
-
 @REM 빌드할 모드입니다.
-SET mode=%2
+SET mode=%1
 
 @REM 빌드 모드의 유효성을 검사합니다.
 if "%mode%" == "Debug" (
