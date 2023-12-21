b76deefb77b932f796e0ef97749088da5d9240d4
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-12-21 09:21:13 +0900

    빌드 스크립트를 CMake 기반으로 빌드하도록 변경

--- GameEngine/Script/BuildSolution.bat
+++ GameEngine/Script/BuildSolution.bat
@@ -1,41 +1,67 @@
 @echo off
-setlocal enabledelayedexpansion
 
-@REM 게임 엔진 경로입니다.
-SET engine_path=%~dp0..\
+echo Start "BuildSolution.bat" script...
 
-@REM vswhere 실행 파일 경로입니다.
-SET vswhere_path=%engine_path%Bin\vswhere.exe
+@REM CMake가 설치되었는지 확인합니다.
+@echo off
+where cmake > nul 2>&1
+if %ERRORLEVEL% EQU 0 (
+    echo CMake is installed on this system...
+) else (
+    echo CMake is not installed on this system...
+    GOTO:EOF
+)
 
-@REM 빌드할 프로젝트 이름입니다.
-SET project=%1
+@REM Visual Studio 버전입니다. 
+SET vs=%1
+
+@REM visual studio 버전을 검사합니다. 2019 혹은 2022입니다.
+if "%vs%" == "vs2019" (
+    SET visualstudio="Visual Studio 16 2019"
+    echo Visual Studio version is "%vs%"...
+) else if "%vs%" == "vs2022" (
+    SET visualstudio="Visual Studio 17 2022"
+    echo Visual Studio version is "%vs%"...
+) else (
+    echo Visual Studio version "%vs%" is illegal...
+    GOTO:EOF
+)
+
+@REM 프로젝트가 존재하지 않으면 프로젝트를 생성합니다.
+if not exist %~dp0..\..\Solution (
+    echo Start generate Visual Studio Solution...
+    mkdir %~dp0..\..\Solution
+
+    pushd %~dp0
+    pushd %~dp0..\..\Solution
+
+    cmake .. -G %visualstudio% -A "x64"
+
+    popd
+    popd
+)
 
 @REM 빌드할 모드입니다.
 SET mode=%2
 
-@REM 빌드할 솔루션 경로입니다.
-SET solution_path=Solution\%project%.sln
-
-@REM 프로젝트 이름의 유효성을 검사합니다.
-if "%project%" == "" (
-    echo project name is empty...
-    GOTO:EOF
-)
-
 @REM 빌드 모드의 유효성을 검사합니다.
 if "%mode%" == "Debug" (
-    echo Build %mode% mode game...
+    echo Build "%mode%" mode...
 ) else if "%mode%" == "Release" (
-    echo Build %mode% mode game...
-) else if "%mode%" == "Shipping" (
-    echo Build %mode% mode game...
+    echo Build "%mode%" mode...
+) else if "%mode%" == "RelWithDebInfo" (
+    echo Build "%mode%" mode...
+) else if "%mode%" == "MinSizeRel" (
+    echo Build "%mode%" mode...
 ) else (
-    echo %mode% is illegal mode...
+    echo "%mode%" is illegal mode...
     GOTO:EOF
 )
 
-for /f "usebackq tokens=*" %%i in (`%vswhere_path% -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
-    set path="%%i"
-)
+pushd %~dp0
+pushd %~dp0..\..\Solution
 
-%path% %solution_path% -property:Configuration=%mode% -target:Rebuild
+cmake --build . --config %mode% --clean-first --parallel  
+
+popd
+popd
