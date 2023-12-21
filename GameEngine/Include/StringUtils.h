9e8c64ef52ca536f53f135588c84dc7d19ce2a71
CHOIJIONE\choij <choijione02@gmail.com>
2023-12-21 16:34:45 +0900

    형식화된 문자열을 생성하는 기능 추가
    - std::string와 std::wstring에 대해서 동작하도록 구현

--- GameEngine/Include/StringUtils.h
+++ GameEngine/Include/StringUtils.h
@@ -11,6 +11,38 @@
 class StringUtils
 {
 public:
+	/**
+	 * @brief 형식화된 문자열을 반환합니다.
+	 *
+	 * @note
+	 * - 이 메서드는 snprintf와 같은 함수를 대체하기 위한 것입니다.
+	 * - 문자열의 길이는 최대 1024 입니다.
+	 *
+	 * @param format 형식 문자열입니다.
+	 * @param ... 문자열의 형식에 대응하는 가변인자입니다.
+	 *
+	 * @return 형식화된 표준 문자열을 반환합니다.
+	 *
+	 * @see https://learn.microsoft.com/ko-kr/cpp/c-runtime-library/reference/vsnprintf-vsnprintf-vsnprintf-l-vsnwprintf-vsnwprintf-l?view=msvc-170
+	 */
+	static std::string PrintF(const char* format, ...);
+
+
+	/**
+	 * @brief 형식화된 문자열을 반환합니다.
+	 *
+	 * @note
+	 * - 이 메서드는 snprintf와 같은 함수를 대체하기 위한 것입니다.
+	 * - 문자열의 길이는 최대 1024 입니다.
+	 *
+	 * @param format 형식 문자열입니다.
+	 * @param ... 문자열의 형식에 대응하는 가변인자입니다.
+	 *
+	 * @return 형식화된 표준 문자열을 반환합니다.
+	 *
+	 * @see https://learn.microsoft.com/ko-kr/cpp/c-runtime-library/reference/vsnprintf-vsnprintf-vsnprintf-l-vsnwprintf-vsnwprintf-l?view=msvc-170
+	 */
+	static std::wstring PrintF(const wchar_t* format, ...);
 
 
 private:
--- GameEngine/Source/StringUtils.cpp
+++ GameEngine/Source/StringUtils.cpp
@@ -2,3 +2,23 @@
 
 char StringUtils::charBuffer_[STRING_BUFFER_SIZE];
 wchar_t StringUtils::wcharBuffer_[STRING_BUFFER_SIZE];
+
+std::string StringUtils::PrintF(const char* format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	int32_t size = vsnprintf(charBuffer_, STRING_BUFFER_SIZE, format, args);
+	va_end(args);
+
+	return std::string(charBuffer_, size);
+}
+
+std::wstring StringUtils::PrintF(const wchar_t* format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	int32_t size = _vsnwprintf_s(wcharBuffer_, STRING_BUFFER_SIZE, format, args);
+	va_end(args);
+
+	return std::wstring(wcharBuffer_, size);
+}
