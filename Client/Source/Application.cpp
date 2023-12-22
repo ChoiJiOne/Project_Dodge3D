9f5169162ac00fe631a05de6b1a7c293517b3d82
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-12-23 06:40:05 +0900

    Application.cpp의 윈도우 메시지 처리를 수행하는 함수 삭제
    - InputManager에 구현되어 있으므로 삭제

--- Client/Source/Application.cpp
+++ Client/Source/Application.cpp
@@ -2,33 +2,15 @@
 #include <string>
 #include <windows.h>
 
+#include "InputManager.h"
 #include "RenderManager.h"
 #include "Window.h"
 
-Window window;
-
-LRESULT CALLBACK WindowProc(HWND windowHandle, uint32_t message, WPARAM wParam, LPARAM lParam)
-{
-	switch (message)
-	{
-	case WM_CREATE:
-		break;
-
-	case WM_CLOSE:
-		break;
-
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	}
-
-	return DefWindowProcW(windowHandle, message, wParam, lParam);
-}
-
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
-	Window::RegisterWindowClass(L"ProjectA", WindowProc);
+	Window::RegisterWindowClass(L"ProjectA", InputManager::ProcessWindowMessage);
 
+	Window window;
 	window.Create(L"ProjectA", 200, 200, 800, 600, true, false);
 	RenderManager::Get().SetRenderTargetWindow(&window);
 
