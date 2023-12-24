4295496cb6ec6908b23136083b855adba5a3f3f0
CHOIJIONE\choij <choijione02@gmail.com>
2023-12-24 19:32:50 +0900

    명령행 인자를 파싱하는 기능 추가
    - 실행 파일 경로를 나타내는 멤버 변수는 삭제

--- GameEngine/Source/CommandLineUtils.cpp
+++ GameEngine/Source/CommandLineUtils.cpp
@@ -8,7 +8,6 @@
 #include <regex>
 
 bool CommandLineUtils::bIsInitialized_ = false;
-std::wstring CommandLineUtils::executePath_;
 std::unordered_map<std::wstring, std::wstring> CommandLineUtils::argumentMaps_;
 
 void CommandLineUtils::Parse(const wchar_t* commandLine)
@@ -16,11 +15,26 @@ void CommandLineUtils::Parse(const wchar_t* commandLine)
 	ASSERT(!bIsInitialized_, "Parse method can only be called once after the program starts...");
 
 	std::vector<std::wstring> arguments = StringUtils::Split(commandLine, L" ");
+	
+	std::wregex pattern(L"^[^=]+=[^=]+$");
+	for (std::size_t index = 0; index < arguments.size(); ++index)
+	{
+		if (std::regex_match(arguments[index], pattern))
+		{
+			std::vector<std::wstring> keyValue = StringUtils::Split(arguments[index], L"=");
+			argumentMaps_.insert({ keyValue.front(), keyValue.back() });
+		}
+	}
 
-	executePath_ = arguments[0];
+	bIsInitialized_ = true;
+}
+
+void CommandLineUtils::ParseArguments(const std::vector<std::wstring>& arguments)
+{
+	ASSERT(!bIsInitialized_, "Parse method can only be called once after the program starts...");
 
 	std::wregex pattern(L"^[^=]+=[^=]+$");
-	for (std::size_t index = 1; index < arguments.size(); ++index)
+	for (std::size_t index = 0; index < arguments.size(); ++index)
 	{
 		if (std::regex_match(arguments[index], pattern))
 		{
