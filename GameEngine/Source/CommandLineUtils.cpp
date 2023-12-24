e84ca2311d0b0877f0e331f499e87678bfdf79c5
CHOIJIONE\choij <choijione02@gmail.com>
2023-12-24 19:34:14 +0900

    명령행을 파싱하는 기능 내부에서 ParseArguments를 호출하도록 변경

--- GameEngine/Source/CommandLineUtils.cpp
+++ GameEngine/Source/CommandLineUtils.cpp
@@ -12,21 +12,8 @@ std::unordered_map<std::wstring, std::wstring> CommandLineUtils::argumentMaps_;
 
 void CommandLineUtils::Parse(const wchar_t* commandLine)
 {
-	ASSERT(!bIsInitialized_, "Parse method can only be called once after the program starts...");
-
 	std::vector<std::wstring> arguments = StringUtils::Split(commandLine, L" ");
-	
-	std::wregex pattern(L"^[^=]+=[^=]+$");
-	for (std::size_t index = 0; index < arguments.size(); ++index)
-	{
-		if (std::regex_match(arguments[index], pattern))
-		{
-			std::vector<std::wstring> keyValue = StringUtils::Split(arguments[index], L"=");
-			argumentMaps_.insert({ keyValue.front(), keyValue.back() });
-		}
-	}
-
-	bIsInitialized_ = true;
+	ParseArguments(arguments);
 }
 
 void CommandLineUtils::ParseArguments(const std::vector<std::wstring>& arguments)
