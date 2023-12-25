40b62435908cbfe922162c788d238b0473dfb8f1
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-12-25 10:01:29 +0900

    최상위 예외처리기 등록/해제  및 미니 덤프 파일 생성 기능 추가

--- GameEngine/Include/WindowsCrashUtils.h
+++ GameEngine/Include/WindowsCrashUtils.h
@@ -1,5 +1,55 @@
 #pragma once
 
-#include <dbghelp.h>
 #include <string>
 #include <windows.h>
+
+
+/**
+ * @brief Windows 크래시에 대한 확장 기능을 제공합니다.
+ * 
+ * @note 이 클래스의 모든 멤버 변수와 메서드는 모두 static입니다.
+ */
+class WindowsCrashUtils
+{
+public:
+	/**
+	 * @brief 최상위 예외 처리기를 등록합니다.
+	 *
+	 * @see https://learn.microsoft.com/ko-kr/windows/win32/api/errhandlingapi/nf-errhandlingapi-setunhandledexceptionfilter
+	 */
+	static void RegisterExceptionFilter();
+
+
+	/**
+	 * @brief 최상위 예외 처리기 등록을 해제합니다.
+	 */
+	static void UnregisterExceptionFilter();
+
+
+	/**
+	 * @brief 크래시를 감지하고 에러 처리를 수행합니다.
+	 * 
+	 * @param exceptionPtr 예외와 컨텍스트 레코드입니다.
+	 * 
+	 * @return EXCEPTION_EXECUTE_HANDLER 값을 반환합니다.
+	 */
+	static LONG WINAPI DetectApplicationCrash(EXCEPTION_POINTERS* exceptionPtr);
+
+
+private:
+	/**
+	 * @brief 크래시가 감지된 시스템 시간값을 얻습니다.
+	 * 
+	 * @return 시스템 시간값을 반환합니다.
+	 */
+	static std::wstring GetCrashSystemTime();
+
+	
+private:
+	/**
+	 * @brief 크래시와 관련된 파일들을 저장할 경로입니다.
+	 * 
+	 * @note 크래시와 관련된 파일은 미니 덤프 파일과 콜스택 기록 파일입니다.
+	 */
+	static std::wstring crashInfoSavePath_;
+};
--- GameEngine/Source/WindowsCrashUtils.cpp
+++ GameEngine/Source/WindowsCrashUtils.cpp
@@ -1,3 +1,73 @@
 #include "WindowsCrashUtils.h"
 
 #include "StringUtils.h"
+#include "WindowsAssertion.h"
+
+#include <dbghelp.h>
+#include <pathcch.h>
+
+std::wstring WindowsCrashUtils::crashInfoSavePath_;
+
+LPTOP_LEVEL_EXCEPTION_FILTER topLevelExceptionFilter;
+
+void WindowsCrashUtils::RegisterExceptionFilter()
+{
+	topLevelExceptionFilter = SetUnhandledExceptionFilter(DetectApplicationCrash);
+
+	wchar_t* bufferPtr = StringUtils::GetWideCharBufferPtr();
+	WINDOWS_ASSERT(GetModuleFileNameW(nullptr, bufferPtr, StringUtils::STRING_BUFFER_SIZE), "failed to get execute file name...");
+	ASSERT(SUCCEEDED(PathCchRemoveFileSpec(bufferPtr, StringUtils::STRING_BUFFER_SIZE)), "failed to remove execute file name...");
+
+	crashInfoSavePath_ = StringUtils::PrintF(L"%s\\", bufferPtr);
+}
+
+void WindowsCrashUtils::UnregisterExceptionFilter()
+{
+	SetUnhandledExceptionFilter(topLevelExceptionFilter);
+}
+
+LONG WINAPI WindowsCrashUtils::DetectApplicationCrash(EXCEPTION_POINTERS* exceptionPtr)
+{
+	std::wstring systemTime = GetCrashSystemTime();
+	std::wstring minidump = StringUtils::PrintF(L"%sWindows-%s-Minidump.dmp", crashInfoSavePath_.c_str(), systemTime.c_str());
+
+	HANDLE dumpFileHandle = CreateFileW(minidump.c_str(), GENERIC_WRITE, FILE_SHARE_WRITE, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+	if (dumpFileHandle == INVALID_HANDLE_VALUE)
+	{
+		return EXCEPTION_EXECUTE_HANDLER;
+	}
+
+	_MINIDUMP_EXCEPTION_INFORMATION exception;
+	exception.ThreadId = GetCurrentThreadId();
+	exception.ExceptionPointers = exceptionPtr;
+	exception.ClientPointers = FALSE;
+
+	MiniDumpWriteDump(
+		GetCurrentProcess(),
+		GetCurrentProcessId(),
+		dumpFileHandle,
+		MiniDumpWithFullMemory,
+		&exception,
+		nullptr,
+		nullptr
+	);
+
+	CloseHandle(dumpFileHandle);
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+
+std::wstring WindowsCrashUtils::GetCrashSystemTime()
+{
+	SYSTEMTIME currentSystemTime;
+	GetLocalTime(&currentSystemTime);
+
+	return StringUtils::PrintF(
+		L"%4d-%02d-%02d-%02d-%02d-%02d",
+		static_cast<int32_t>(currentSystemTime.wYear),
+		static_cast<int32_t>(currentSystemTime.wMonth),
+		static_cast<int32_t>(currentSystemTime.wDay),
+		static_cast<int32_t>(currentSystemTime.wHour),
+		static_cast<int32_t>(currentSystemTime.wMinute),
+		static_cast<int32_t>(currentSystemTime.wSecond)
+	);
+}
