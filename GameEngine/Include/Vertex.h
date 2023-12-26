e19b6784063b3de2d3e6805020e7b0e57ca446de
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-12-27 07:51:30 +0900

    위치와 법선 정보를 가진 정점 추가

--- GameEngine/Include/Vertex.h
+++ GameEngine/Include/Vertex.h
@@ -380,3 +380,136 @@ struct VertexPositionTexture
 	 */
 	Vector2f texture;
 };
+
+
+/**
+ * @brief 위치와 법선 정보를 가진 정점(Vertex)입니다.
+ */
+struct VertexPositionNormal
+{
+	/**
+	 * @brief 위치와 법선 정보를 가진 정점의 기본 생성자입니다.
+	 */
+	VertexPositionNormal() noexcept
+		: position(0.0f, 0.0f, 0.0f)
+		, normal(0.0f, 0.0f, 0.0f) {}
+
+
+	/**
+	 * @brief 위치와 법선 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param p 정점의 위치입니다.
+	 * @param n 정점의 법선입니다.
+	 */
+	VertexPositionNormal(Vector3f&& p, Vector3f&& n) noexcept
+		: position(p)
+		, normal(n) {}
+
+
+	/**
+	 * @brief 위치와 법선 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param p 정점의 위치입니다.
+	 * @param n 정점의 법선입니다.
+	 */
+	VertexPositionNormal(const Vector3f& p, const Vector3f& n) noexcept
+		: position(p)
+		, normal(n) {}
+
+
+	/**
+	 * @brief 위치와 법선 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param x 위치의 X좌표입니다.
+	 * @param y 위치의 Y좌표입니다.
+	 * @param z 위치의 Z좌표입니다.
+	 * @param nx 법선의 X좌표입니다.
+	 * @param ny 법선의 Y좌표입니다.
+	 * @param nz 법선의 Z좌표입니다.
+	 */
+	VertexPositionNormal(
+		float x, float y, float z,
+		float nx, float ny, float nz
+	) noexcept
+		: position(x, y, z)
+		, normal(nx, ny, nz) {}
+
+
+	/**
+	 * @brief 위치와 법선 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param instance 복사할 정점 인스턴스입니다.
+	 */
+	VertexPositionNormal(VertexPositionNormal&& instance) noexcept
+		: position(instance.position)
+		, normal(instance.normal) {}
+
+
+	/**
+	 * @brief 위치와 법선 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param instance 복사할 정점 인스턴스입니다.
+	 */
+	VertexPositionNormal(const VertexPositionNormal& instance) noexcept
+		: position(instance.position)
+		, normal(instance.normal) {}
+
+
+	/**
+	 * @brief 위치와 법선 정보를 가진 정점의 대입 연산자입니다.
+	 *
+	 * @param instance 대입할 정점 인스턴스입니다.
+	 *
+	 * @return 대입한 객체의 참조자를 반환합니다.
+	 */
+	VertexPositionNormal& operator=(VertexPositionNormal&& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		position = instance.position;
+		normal = instance.normal;
+
+		return *this;
+	}
+
+
+	/**
+	 * @brief 위치와 법선 정보를 가진 정점의 대입 연산자입니다.
+	 *
+	 * @param instance 대입할 정점 인스턴스입니다.
+	 *
+	 * @return 대입한 객체의 참조자를 반환합니다.
+	 */
+	VertexPositionNormal& operator=(const VertexPositionNormal& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		position = instance.position;
+		normal = instance.normal;
+
+		return *this;
+	}
+
+
+	/**
+	 * @brief 정점의 바이트 보폭 값을 얻습니다.
+	 *
+	 * @return 정점의 파이트 보폭(Stride) 값을 반환합니다.
+	 */
+	static uint32_t GetStride()
+	{
+		return sizeof(VertexPositionNormal);
+	}
+
+
+	/**
+	 * @brief 정점의 위치입니다.
+	 */
+	Vector3f position;
+
+
+	/**
+	 * @brief 정점의 법선 좌표입니다.
+	 */
+	Vector3f normal;
+};
