4a9f25f407fb3a9d7a1e7f8b50cbf8716b2262c8
CHOIJIONE\choij <choijione02@gmail.com>
2023-12-26 18:17:01 +0900

    템플릿 기반의 두 값 중 최대/최소 값을 얻는 기능 추가

--- GameEngine/Include/MathUtils.h
+++ GameEngine/Include/MathUtils.h
@@ -111,4 +111,34 @@ public:
 	{
 		return (std::fabs(value) <= epsilon);
 	}
+
+
+	/**
+	 * @brief 두 값중 큰 값을 반환합니다.
+	 *
+	 * @param lhs 크기를 비교할 값 중 왼쪽 값입니다.
+	 * @param rhs 크기를 비교할 값 중 오른쪽 값입니다.
+	 *
+	 * @return 두 값 중 큰 값을 반환합니다.
+	 */
+	template <typename T>
+	static inline T Max(const T& lhs, const T& rhs)
+	{
+		return (lhs < rhs ? rhs : lhs);
+	}
+
+
+	/**
+	 * @brief 두 값중 작은 값을 반환합니다.
+	 *
+	 * @param lhs 크기를 비교할 값 중 왼쪽 값입니다.
+	 * @param rhs 크기를 비교할 값 중 오른쪽 값입니다.
+	 *
+	 * @return 두 값 중 작은 값을 반환합니다.
+	 */
+	template <typename T>
+	static inline T Min(const T& lhs, const T& rhs)
+	{
+		return (lhs < rhs ? lhs : rhs);
+	}
 };
