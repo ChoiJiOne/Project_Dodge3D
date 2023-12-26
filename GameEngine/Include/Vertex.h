ed2f689df110a6b3bacbd4c0006ce4887ee2eb01
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-12-27 07:37:12 +0900

    위치와 텍스처 정보를 가진 정점 추가

--- GameEngine/Include/Vertex.h
+++ GameEngine/Include/Vertex.h
@@ -248,3 +248,135 @@ struct VertexPositionColor
 	 */
 	Vector4f color;
 };
+
+
+/**
+ * @brief 위치와 텍스처 정보를 가진 정점(Vertex)입니다.
+ */
+struct VertexPositionTexture
+{
+	/**
+	 * @brief 위치와 텍스처 정보를 가진 정점의 기본 생성자입니다.
+	 */
+	VertexPositionTexture() noexcept
+		: position(0.0f, 0.0f, 0.0f)
+		, texture(0.0f, 0.0f) {}
+
+
+	/**
+	 * @brief 위치와 텍스처 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param p 정점의 위치입니다.
+	 * @param uv 정점의 텍스처 위치입니다.
+	 */
+	VertexPositionTexture(Vector3f&& p, Vector2f&& uv) noexcept
+		: position(p)
+		, texture(uv) {}
+
+
+	/**
+	 * @brief 위치와 텍스처 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param p 정점의 위치입니다.
+	 * @param uv 정점의 텍스처 위치입니다.
+	 */
+	VertexPositionTexture(const Vector3f& p, const Vector2f& uv) noexcept
+		: position(p)
+		, texture(uv) {}
+
+
+	/**
+	 * @brief 위치와 텍스처 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param x 위치의 X좌표입니다.
+	 * @param y 위치의 Y좌표입니다.
+	 * @param z 위치의 Z좌표입니다.
+	 * @param u 텍스처 위치의 U좌표입니다.
+	 * @param v 텍스처 위치의 V좌표입니다.
+	 */
+	VertexPositionTexture(
+		float x, float y, float z,
+		float u, float v
+	) noexcept
+		: position(x, y, z)
+		, texture(u, v) {}
+
+
+	/**
+	 * @brief 위치와 텍스처 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param instance 복사할 정점 인스턴스입니다.
+	 */
+	VertexPositionTexture(VertexPositionTexture&& instance) noexcept
+		: position(instance.position)
+		, texture(instance.texture) {}
+
+
+	/**
+	 * @brief 위치와 텍스처 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param instance 복사할 정점 인스턴스입니다.
+	 */
+	VertexPositionTexture(const VertexPositionTexture& instance) noexcept
+		: position(instance.position)
+		, texture(instance.texture) {}
+
+
+	/**
+	 * @brief 위치와 텍스처 정보를 가진 정점의 대입 연산자입니다.
+	 *
+	 * @param instance 대입할 정점 인스턴스입니다.
+	 *
+	 * @return 대입한 객체의 참조자를 반환합니다.
+	 */
+	VertexPositionTexture& operator=(VertexPositionTexture&& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		position = instance.position;
+		texture = instance.texture;
+
+		return *this;
+	}
+
+
+	/**
+	 * @brief 위치와 텍스처 정보를 가진 정점의 대입 연산자입니다.
+	 *
+	 * @param instance 대입할 정점 인스턴스입니다.
+	 *
+	 * @return 대입한 객체의 참조자를 반환합니다.
+	 */
+	VertexPositionTexture& operator=(const VertexPositionTexture& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		position = instance.position;
+		texture = instance.texture;
+
+		return *this;
+	}
+
+
+	/**
+	 * @brief 정점의 바이트 보폭 값을 얻습니다.
+	 *
+	 * @return 정점의 파이트 보폭(Stride) 값을 반환합니다.
+	 */
+	static uint32_t GetStride()
+	{
+		return sizeof(VertexPositionTexture);
+	}
+
+
+	/**
+	 * @brief 정점의 위치입니다.
+	 */
+	Vector3f position;
+
+
+	/**
+	 * @brief 정점의 텍스처 좌표입니다.
+	 */
+	Vector2f texture;
+};
