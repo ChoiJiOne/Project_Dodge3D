77cabb5bb5acd1793fcc2d40905df7c0d65841f6
CHOIJIONE\choij <choijione02@gmail.com>
2023-12-28 11:41:16 +0900

    여러 개의 정점을 하나의 형식으로 통일

--- GameEngine/Include/Vertex.h
+++ GameEngine/Include/Vertex.h
@@ -6,202 +6,104 @@
 
 
 /**
- * @brief 위치 정보를 가진 정점(Vertex)입니다.
+ * @brief 정점(Vertex)입니다.
  */
-struct VertexPosition
+struct Vertex
 {
 	/**
 	 * @brief 위치 정보를 가진 정점의 기본 생성자입니다.
 	 */
-	VertexPosition() noexcept
-		: position(0.0f, 0.0f, 0.0f) {}
-
-
-	/**
-	 * @brief 위치 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param p 정점의 위치입니다.
-	 */
-	VertexPosition(Vector3f&& p) noexcept
-		: position(p) {}
-
-
-	/**
-	 * @brief 위치 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param p 정점의 위치입니다.
-	 */
-	VertexPosition(const Vector3f& p) noexcept
-		: position(p) {}
-
-
-	/**
-	 * @brief 위치 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param x 위치의 X좌표입니다.
-	 * @param y 위치의 Y좌표입니다.
-	 * @param z 위치의 Z좌표입니다.
-	 */
-	VertexPosition(float x, float y, float z) noexcept
-		: position(x, y, z) {}
-
-
-	/**
-	 * @brief 위치 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param instance 복사할 정점 인스턴스입니다.
-	 */
-	VertexPosition(VertexPosition&& instance) noexcept
-		: position(instance.position) {}
-
-
-	/**
-	 * @brief 위치 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param instance 복사할 정점 인스턴스입니다.
-	 */
-	VertexPosition(const VertexPosition& instance) noexcept
-		: position(instance.position) {}
-
-
-	/**
-	 * @brief 위치 정보를 가진 정점의 대입 연산자입니다.
-	 *
-	 * @param instance 대입할 정점 인스턴스입니다.
-	 *
-	 * @return 대입한 객체의 참조자를 반환합니다.
-	 */
-	VertexPosition& operator=(VertexPosition&& instance) noexcept
-	{
-		if (this == &instance) return *this;
-
-		position = instance.position;
-
-		return *this;
-	}
-
-
-	/**
-	 * @brief 위치 정보를 가진 정점의 대입 연산자입니다.
-	 *
-	 * @param instance 대입할 정점 인스턴스입니다.
-	 *
-	 * @return 대입한 객체의 참조자를 반환합니다.
-	 */
-	VertexPosition& operator=(const VertexPosition& instance) noexcept
-	{
-		if (this == &instance) return *this;
-
-		position = instance.position;
-
-		return *this;
-	}
-
-
-	/**
-	 * @brief 정점의 바이트 보폭 값을 얻습니다.
-	 *
-	 * @return 정점의 파이트 보폭(Stride) 값을 반환합니다.
-	 */
-	static uint32_t GetStride()
-	{
-		return sizeof(VertexPosition);
-	}
-
-
-	/**
-	 * @brief 정점의 위치입니다.
-	 */
-	Vector3f position;
-};
-
-
-/**
- * @brief 위치와 색상 정보를 가진 정점(Vertex)입니다.
- */
-struct VertexPositionColor
-{
-	/**
-	 * @brief 위치와 색상 정보를 가진 정점의 기본 생성자입니다.
-	 */
-	VertexPositionColor() noexcept
+	Vertex() noexcept
 		: position(0.0f, 0.0f, 0.0f)
+		, normal(0.0f, 0.0f, 0.0f)
+		, texture(0.0f, 0.0f)
+		, tangent(0.0f, 0.0f, 0.0f)
+		, bitangent(0.0f, 0.0f, 0.0f)
 		, color(0.0f, 0.0f, 0.0f, 0.0f) {}
 
 
 	/**
-	 * @brief 위치와 색상 정보를 가진 정점의 생성자입니다.
+	 * @brief 위치 정보를 가진 정점의 생성자입니다.
 	 *
 	 * @param p 정점의 위치입니다.
+	 * @param n 정점의 법선 벡터입니다.
+	 * @param uv 정점의 텍셀입니다.
+	 * @param t 정점의 접선 벡터입니다.
+	 * @param b 정점의 쌍벡터입니다.
 	 * @param c 정점의 색상입니다.
 	 */
-	VertexPositionColor(Vector3f&& p, Vector4f&& c) noexcept
-		: position(p)
+	Vertex(Vector3f&& p, Vector3f&& n, Vector2f&& uv, Vector3f&& t, Vector3f&& b, Vector4f&& c) noexcept
+		: position(p) 
+		, normal(n)
+		, texture(uv)
+		, tangent(t)
+		, bitangent(b)
 		, color(c) {}
 
 
 	/**
-	 * @brief 위치와 색상 정보를 가진 정점의 생성자입니다.
+	 * @brief 위치 정보를 가진 정점의 생성자입니다.
 	 *
 	 * @param p 정점의 위치입니다.
+	 * @param n 정점의 법선 벡터입니다.
+	 * @param uv 정점의 텍셀입니다.
+	 * @param t 정점의 접선 벡터입니다.
+	 * @param b 정점의 쌍벡터입니다.
 	 * @param c 정점의 색상입니다.
 	 */
-	VertexPositionColor(const Vector3f& p, const Vector4f& c) noexcept
+	Vertex(const Vector3f& p, const Vector3f& n, const Vector2f& uv, const Vector3f& t, const Vector3f& b, const Vector4f& c) noexcept
 		: position(p)
+		, normal(n)
+		, texture(uv)
+		, tangent(t)
+		, bitangent(b)
 		, color(c) {}
 
-
+	
 	/**
-	 * @brief 위치와 색상 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param x 위치의 X좌표입니다.
-	 * @param y 위치의 Y좌표입니다.
-	 * @param z 위치의 Z좌표입니다.
-	 * @param r 정점 색상의 R입니다.
-	 * @param g 정점 색상의 G입니다.
-	 * @param b 정점 색상의 B입니다.
-	 * @param a 정점 색상의 A입니다.
-	 */
-	VertexPositionColor(
-		float x, float y, float z,
-		float r, float g, float b, float a
-	) noexcept
-		: position(x, y, z)
-		, color(r, g, b, a) {}
-
-
-	/**
-	 * @brief 위치와 색상 정보를 가진 정점의 생성자입니다.
+	 * @brief 위치 정보를 가진 정점의 생성자입니다.
 	 *
 	 * @param instance 복사할 정점 인스턴스입니다.
 	 */
-	VertexPositionColor(VertexPositionColor&& instance) noexcept
+	Vertex(Vertex&& instance) noexcept
 		: position(instance.position)
+		, normal(instance.normal)
+		, texture(instance.texture)
+		, tangent(instance.tangent)
+		, bitangent(instance.bitangent)
 		, color(instance.color) {}
 
 
 	/**
-	 * @brief 위치와 색상 정보를 가진 정점의 생성자입니다.
+	 * @brief 위치 정보를 가진 정점의 생성자입니다.
 	 *
 	 * @param instance 복사할 정점 인스턴스입니다.
 	 */
-	VertexPositionColor(const VertexPositionColor& instance) noexcept
+	Vertex(const Vertex& instance) noexcept
 		: position(instance.position)
+		, normal(instance.normal)
+		, texture(instance.texture)
+		, tangent(instance.tangent)
+		, bitangent(instance.bitangent)
 		, color(instance.color) {}
 
 
 	/**
-	 * @brief 위치와 색상 정보를 가진 정점의 대입 연산자입니다.
+	 * @brief 위치 정보를 가진 정점의 대입 연산자입니다.
 	 *
 	 * @param instance 대입할 정점 인스턴스입니다.
 	 *
 	 * @return 대입한 객체의 참조자를 반환합니다.
 	 */
-	VertexPositionColor& operator=(VertexPositionColor&& instance) noexcept
+	Vertex& operator=(Vertex&& instance) noexcept
 	{
 		if (this == &instance) return *this;
 
 		position = instance.position;
+		normal = instance.normal;
+		texture = instance.texture;
+		tangent = instance.tangent;
+		bitangent = instance.bitangent;
 		color = instance.color;
 
 		return *this;
@@ -209,17 +111,21 @@ struct VertexPositionColor
 
 
 	/**
-	 * @brief 위치와 색상 정보를 가진 정점의 대입 연산자입니다.
+	 * @brief 위치 정보를 가진 정점의 대입 연산자입니다.
 	 *
 	 * @param instance 대입할 정점 인스턴스입니다.
 	 *
 	 * @return 대입한 객체의 참조자를 반환합니다.
 	 */
-	VertexPositionColor& operator=(const VertexPositionColor& instance) noexcept
+	Vertex& operator=(const Vertex& instance) noexcept
 	{
 		if (this == &instance) return *this;
 
 		position = instance.position;
+		normal = instance.normal;
+		texture = instance.texture;
+		tangent = instance.tangent;
+		bitangent = instance.bitangent;
 		color = instance.color;
 
 		return *this;
@@ -233,7 +139,7 @@ struct VertexPositionColor
 	 */
 	static uint32_t GetStride()
 	{
-		return sizeof(VertexPositionColor);
+		return sizeof(Vertex);
 	}
 
 
@@ -241,275 +147,34 @@ struct VertexPositionColor
 	 * @brief 정점의 위치입니다.
 	 */
 	Vector3f position;
+
+
+	/**
+	 * @brief 정점의 법선 벡터입니다.
+	 */
+	Vector3f normal;
+
+
+	/**
+	 * @brief 정점의 텍셀입니다.
+	 */
+	Vector2f texture;
+
+
+	/**
+	 * @brief 정점의 접선 벡터입니다.
+	 */
+	Vector3f tangent;
+
+
+	/**
+	 * @brief 정점의 쌍벡터입니다.
+	 */
+	Vector3f bitangent;
 
 
 	/**
 	 * @brief 정점의 색상입니다.
 	 */
 	Vector4f color;
-};
-
-
-/**
- * @brief 위치와 텍스처 정보를 가진 정점(Vertex)입니다.
- */
-struct VertexPositionTexture
-{
-	/**
-	 * @brief 위치와 텍스처 정보를 가진 정점의 기본 생성자입니다.
-	 */
-	VertexPositionTexture() noexcept
-		: position(0.0f, 0.0f, 0.0f)
-		, texture(0.0f, 0.0f) {}
-
-
-	/**
-	 * @brief 위치와 텍스처 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param p 정점의 위치입니다.
-	 * @param uv 정점의 텍스처 위치입니다.
-	 */
-	VertexPositionTexture(Vector3f&& p, Vector2f&& uv) noexcept
-		: position(p)
-		, texture(uv) {}
-
-
-	/**
-	 * @brief 위치와 텍스처 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param p 정점의 위치입니다.
-	 * @param uv 정점의 텍스처 위치입니다.
-	 */
-	VertexPositionTexture(const Vector3f& p, const Vector2f& uv) noexcept
-		: position(p)
-		, texture(uv) {}
-
-
-	/**
-	 * @brief 위치와 텍스처 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param x 위치의 X좌표입니다.
-	 * @param y 위치의 Y좌표입니다.
-	 * @param z 위치의 Z좌표입니다.
-	 * @param u 텍스처 위치의 U좌표입니다.
-	 * @param v 텍스처 위치의 V좌표입니다.
-	 */
-	VertexPositionTexture(
-		float x, float y, float z,
-		float u, float v
-	) noexcept
-		: position(x, y, z)
-		, texture(u, v) {}
-
-
-	/**
-	 * @brief 위치와 텍스처 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param instance 복사할 정점 인스턴스입니다.
-	 */
-	VertexPositionTexture(VertexPositionTexture&& instance) noexcept
-		: position(instance.position)
-		, texture(instance.texture) {}
-
-
-	/**
-	 * @brief 위치와 텍스처 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param instance 복사할 정점 인스턴스입니다.
-	 */
-	VertexPositionTexture(const VertexPositionTexture& instance) noexcept
-		: position(instance.position)
-		, texture(instance.texture) {}
-
-
-	/**
-	 * @brief 위치와 텍스처 정보를 가진 정점의 대입 연산자입니다.
-	 *
-	 * @param instance 대입할 정점 인스턴스입니다.
-	 *
-	 * @return 대입한 객체의 참조자를 반환합니다.
-	 */
-	VertexPositionTexture& operator=(VertexPositionTexture&& instance) noexcept
-	{
-		if (this == &instance) return *this;
-
-		position = instance.position;
-		texture = instance.texture;
-
-		return *this;
-	}
-
-
-	/**
-	 * @brief 위치와 텍스처 정보를 가진 정점의 대입 연산자입니다.
-	 *
-	 * @param instance 대입할 정점 인스턴스입니다.
-	 *
-	 * @return 대입한 객체의 참조자를 반환합니다.
-	 */
-	VertexPositionTexture& operator=(const VertexPositionTexture& instance) noexcept
-	{
-		if (this == &instance) return *this;
-
-		position = instance.position;
-		texture = instance.texture;
-
-		return *this;
-	}
-
-
-	/**
-	 * @brief 정점의 바이트 보폭 값을 얻습니다.
-	 *
-	 * @return 정점의 파이트 보폭(Stride) 값을 반환합니다.
-	 */
-	static uint32_t GetStride()
-	{
-		return sizeof(VertexPositionTexture);
-	}
-
-
-	/**
-	 * @brief 정점의 위치입니다.
-	 */
-	Vector3f position;
-
-
-	/**
-	 * @brief 정점의 텍스처 좌표입니다.
-	 */
-	Vector2f texture;
-};
-
-
-/**
- * @brief 위치와 법선 정보를 가진 정점(Vertex)입니다.
- */
-struct VertexPositionNormal
-{
-	/**
-	 * @brief 위치와 법선 정보를 가진 정점의 기본 생성자입니다.
-	 */
-	VertexPositionNormal() noexcept
-		: position(0.0f, 0.0f, 0.0f)
-		, normal(0.0f, 0.0f, 0.0f) {}
-
-
-	/**
-	 * @brief 위치와 법선 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param p 정점의 위치입니다.
-	 * @param n 정점의 법선입니다.
-	 */
-	VertexPositionNormal(Vector3f&& p, Vector3f&& n) noexcept
-		: position(p)
-		, normal(n) {}
-
-
-	/**
-	 * @brief 위치와 법선 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param p 정점의 위치입니다.
-	 * @param n 정점의 법선입니다.
-	 */
-	VertexPositionNormal(const Vector3f& p, const Vector3f& n) noexcept
-		: position(p)
-		, normal(n) {}
-
-
-	/**
-	 * @brief 위치와 법선 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param x 위치의 X좌표입니다.
-	 * @param y 위치의 Y좌표입니다.
-	 * @param z 위치의 Z좌표입니다.
-	 * @param nx 법선의 X좌표입니다.
-	 * @param ny 법선의 Y좌표입니다.
-	 * @param nz 법선의 Z좌표입니다.
-	 */
-	VertexPositionNormal(
-		float x, float y, float z,
-		float nx, float ny, float nz
-	) noexcept
-		: position(x, y, z)
-		, normal(nx, ny, nz) {}
-
-
-	/**
-	 * @brief 위치와 법선 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param instance 복사할 정점 인스턴스입니다.
-	 */
-	VertexPositionNormal(VertexPositionNormal&& instance) noexcept
-		: position(instance.position)
-		, normal(instance.normal) {}
-
-
-	/**
-	 * @brief 위치와 법선 정보를 가진 정점의 생성자입니다.
-	 *
-	 * @param instance 복사할 정점 인스턴스입니다.
-	 */
-	VertexPositionNormal(const VertexPositionNormal& instance) noexcept
-		: position(instance.position)
-		, normal(instance.normal) {}
-
-
-	/**
-	 * @brief 위치와 법선 정보를 가진 정점의 대입 연산자입니다.
-	 *
-	 * @param instance 대입할 정점 인스턴스입니다.
-	 *
-	 * @return 대입한 객체의 참조자를 반환합니다.
-	 */
-	VertexPositionNormal& operator=(VertexPositionNormal&& instance) noexcept
-	{
-		if (this == &instance) return *this;
-
-		position = instance.position;
-		normal = instance.normal;
-
-		return *this;
-	}
-
-
-	/**
-	 * @brief 위치와 법선 정보를 가진 정점의 대입 연산자입니다.
-	 *
-	 * @param instance 대입할 정점 인스턴스입니다.
-	 *
-	 * @return 대입한 객체의 참조자를 반환합니다.
-	 */
-	VertexPositionNormal& operator=(const VertexPositionNormal& instance) noexcept
-	{
-		if (this == &instance) return *this;
-
-		position = instance.position;
-		normal = instance.normal;
-
-		return *this;
-	}
-
-
-	/**
-	 * @brief 정점의 바이트 보폭 값을 얻습니다.
-	 *
-	 * @return 정점의 파이트 보폭(Stride) 값을 반환합니다.
-	 */
-	static uint32_t GetStride()
-	{
-		return sizeof(VertexPositionNormal);
-	}
-
-
-	/**
-	 * @brief 정점의 위치입니다.
-	 */
-	Vector3f position;
-
-
-	/**
-	 * @brief 정점의 법선 좌표입니다.
-	 */
-	Vector3f normal;
 };
