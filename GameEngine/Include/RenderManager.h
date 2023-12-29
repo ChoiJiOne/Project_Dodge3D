9a81a4234a8bdd9a7c14d5c107ede6af7220d694
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-12-29 09:24:41 +0900

    벡터와 행렬을 사용하기 위한 헤더 선언 추가

--- GameEngine/Include/RenderManager.h
+++ GameEngine/Include/RenderManager.h
@@ -5,6 +5,11 @@
 #include <windows.h>
 
 #include "IManager.h"
+#include "Vector2.h"
+#include "Vector3.h"
+#include "Vector4.h"
+#include "Matrix2x2.h"
+#include "Matrix3x3.h"
 #include "Matrix4x4.h"
 
 class Window;
