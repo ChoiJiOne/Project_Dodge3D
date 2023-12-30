e4a8f0d51839d5bc640efd4b92f078133137ae4a
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2023-12-31 08:27:55 +0900

    기존의 3D 격자의 Mesh를 생성하는 코드 삭제
    - 렌더 매니저의 3D 좌표축과 격자를 그리는 기능으로 대체

--- Client/Source/Application.cpp
+++ Client/Source/Application.cpp
@@ -14,6 +14,7 @@
 #include "ResourceManager.h"
 #include "Shader.h"
 #include "Texture2D.h"
+#include "TTFont.h"
 #include "Vector2.h"
 #include "Vector3.h"
 #include "Vector4.h"
@@ -27,57 +28,11 @@ int32_t height = 600;
 std::wstring shaderPath;
 std::wstring resourcePath;
 
-void CreateAxisGrid(const Vector3f& minPosition, const Vector3f& maxPosition, const float& gap, const Vector4f& color, std::vector<Vertex>& outVertices, std::vector<uint32_t>& outIndices)
-{
-	static Vector4f xAxisColor = Vector4f(1.0f, 0.0f, 0.0f, 1.0f);
-	static Vector4f yAxisColor = Vector4f(0.0f, 1.0f, 0.0f, 1.0f);
-	static Vector4f zAxisColor = Vector4f(0.0f, 0.0f, 1.0f, 1.0f);
-
-	outVertices.resize(0);
-	outIndices.resize(0);
-
-	uint32_t index = 0;
-	Vector4f axisColor;
-	for (float x = minPosition.x; x <= maxPosition.x; x += gap)
-	{
-		axisColor = (x == 0.0f) ? zAxisColor : color;
-
-		outVertices.push_back(Vertex(Vector3f(x, 0.0f, minPosition.z), Vector3f(), Vector2f(), Vector3f(), Vector3f(), axisColor));
-		outIndices.push_back(index++);
-
-		outVertices.push_back(Vertex(Vector3f(x, 0.0f, maxPosition.z), Vector3f(), Vector2f(), Vector3f(), Vector3f(), axisColor));
-		outIndices.push_back(index++);
-	}
-
-	for (float z = minPosition.z; z <= maxPosition.z; z += gap)
-	{
-		Vector4f axisColor = (z == 0.0f) ? xAxisColor : color;
-
-		outVertices.push_back(Vertex(Vector3f(minPosition.x, 0.0f, z), Vector3f(), Vector2f(), Vector3f(), Vector3f(), axisColor));
-		outIndices.push_back(index++);
-
-		outVertices.push_back(Vertex(Vector3f(maxPosition.x, 0.0f, z), Vector3f(), Vector2f(), Vector3f(), Vector3f(), axisColor));
-		outIndices.push_back(index++);
-	}
-
-	outVertices.push_back(Vertex(Vector3f(0.0f, minPosition.y, 0.0f), Vector3f(), Vector2f(), Vector3f(), Vector3f(), yAxisColor));
-	outIndices.push_back(index++);
-
-	outVertices.push_back(Vertex(Vector3f(0.0f, maxPosition.y, 0.0f), Vector3f(), Vector2f(), Vector3f(), Vector3f(), yAxisColor));
-	outIndices.push_back(index++);
-}
-
 void RunApplication()
 {
-	std::vector<Vertex> vertices;
-	std::vector<uint32_t> indices;
-	
-	Shader* shader = ResourceManager::Get().GetResource<Shader>("MeshColorPass");
-
-	Mesh* grid = ResourceManager::Get().CreateResource<Mesh>("Grid");
-	CreateAxisGrid(Vector3f(-100.0f, -100.0f, -100.0f), Vector3f(+100.0f, +100.0f, +100.0f), 1.0f, Vector4f(1.0f, 1.0f, 1.0f, 0.5f), vertices, indices);
-	grid->Initialize(vertices, indices);
-
+	TTFont* font = ResourceManager::Get().CreateResource<TTFont>("Font");
+	font->Initialize(resourcePath + L"SeoulNamsanEB.ttf", 27, 128, 32.0f);
+		
 	while (!bIsDone)
 	{
 		InputManager::Get().Tick();
@@ -87,17 +42,8 @@ void RunApplication()
 
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 		{
-			shader->Bind();
-
-			shader->SetMatrix4x4fParameter("world", Matrix4x4f::GetIdentity());
-			shader->SetMatrix4x4fParameter("view", view);
-			shader->SetMatrix4x4fParameter("projection", projection);
-
-			glBindVertexArray(grid->GetVertexArrayObject());
-			glDrawElements(GL_LINES, grid->GetIndexCount(), GL_UNSIGNED_INT, 0);
-
-			glBindVertexArray(0);
-			shader->Unbind();
+			RenderManager::Get().RenderAxisGrid3D(view, projection, (-100.0f, -100.0f, -100.0f), Vector3f(+100.0f, +100.0f, +100.0f), 1.0f, Vector4f(1.0f, 1.0f, 1.0f, 0.5f));
+			RenderManager::Get().RenderText2D(font, L"Hello, World", Vector2f(400.0f, 300.0f), Vector4f(1.0f, 0.0f, 0.0f, 1.0f));
 		}
 		RenderManager::Get().EndFrame();
 	}
