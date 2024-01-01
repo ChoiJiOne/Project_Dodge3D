ae6610b010a83e5432b45b3562e375d7bdbc4840
CHOIJIONE\choij <choijione02@gmail.com>
2024-01-01 16:43:11 +0900

    GeometryGenerator 관련 코드 삭제

--- Client/Source/Application.cpp
+++ Client/Source/Application.cpp
@@ -7,7 +7,6 @@
 #include "Assertion.h"
 #include "CommandLineUtils.h"
 #include "FileManager.h"
-#include "GeometryGenerator.h"
 #include "InputManager.h"
 #include "MathUtils.h"
 #include "Mesh.h"
@@ -31,50 +30,16 @@ std::wstring resourcePath;
 
 void RunApplication()
 {
-	std::vector<Vertex> vertices;
-	std::vector<uint32_t> indices;
-
-	Mesh* mesh = ResourceManager::Get().CreateResource<Mesh>("Mesh");
-	//GeometryGenerator::CreateXZQuad(2.0f, 2.0f, vertices, indices);
-	GeometryGenerator::CreateCylinder(2.0f, 2.0f, 30, 30, vertices, indices);
-	mesh->Initialize(vertices, indices);
-	
-	Texture2D* texture = ResourceManager::Get().CreateResource<Texture2D>("texture");
-	texture->Initialize(resourcePath + L"container6x6.astc");
-
-	Vector3f currentCameraPosition = Vector3f(0.0f, 0.0f, 10.0f);
 	while (!bIsDone)
 	{
 		InputManager::Get().Tick();
-		if (InputManager::Get().GetVirtualKeyPressState(EVirtualKey::VKEY_LEFT) == EPressState::Held)
-		{
-			currentCameraPosition.x -= 0.1f;
-		}
 
-		if (InputManager::Get().GetVirtualKeyPressState(EVirtualKey::VKEY_RIGHT) == EPressState::Held)
-		{
-			currentCameraPosition.x += 0.1f;
-		}
-
-		Matrix4x4f view = MathUtils::CreateLookAt(currentCameraPosition, Vector3f(0.0f, 0.0f, 0.0f), Vector3f(0.0f, 1.0f, 0.0f));
+		Matrix4x4f view = MathUtils::CreateLookAt(Vector3f(0.0f, 5.0f, 10.0f), Vector3f(0.0f, 0.0f, 0.0f), Vector3f(0.0f, 1.0f, 0.0f));
 		Matrix4x4f projection = MathUtils::CreatePerspective(MathUtils::ToRadian(45.0f), static_cast<float>(width) / static_cast<float>(height), 0.01f, 1000.0f);
 
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 		RenderManager::Get().RenderAxisGrid3D(view, projection, (-100.0f, -100.0f, -100.0f), Vector3f(+100.0f, +100.0f, +100.0f), 1.0f, Vector4f(1.0f, 1.0f, 1.0f, 0.5f));
-		RenderManager::Get().RenderMesh3D(Matrix4x4f::GetIdentity(), view, projection, mesh, texture);
-
-		for (std::size_t index = 0; index < 1; ++index)
-		{
-			const Vector3f& position = vertices[index].position;
-			const Vector3f& normal = vertices[index].normal * 0.5f;
-			const Vector3f& tangent = vertices[index].tangent * 0.5f;
-			const Vector3f& bitangent = vertices[index].bitangent * 0.5f;
-
-			RenderManager::Get().RenderLine3D(view, projection, position, position + tangent, Vector4f(1.0f, 0.0f, 0.0f, 1.0f));
-			RenderManager::Get().RenderLine3D(view, projection, position, position + bitangent, Vector4f(0.0f, 1.0f, 0.0f, 1.0f));
-			RenderManager::Get().RenderLine3D(view, projection, position, position + normal, Vector4f(0.0f, 0.0f, 1.0f, 1.0f));
-		}
-
+		
 		RenderManager::Get().EndFrame();
 	}
 }
