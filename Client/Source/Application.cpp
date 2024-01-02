400a7c2d5bf55fed4b984aa683b8ecaed7f11fd9
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-01-03 07:44:37 +0900

    퐁 반사 모델 기반의 셰이더 작동을 확인하는 테스트 코드 추가

--- Client/Source/Application.cpp
+++ Client/Source/Application.cpp
@@ -8,6 +8,7 @@
 #include "Assertion.h"
 #include "CommandLineUtils.h"
 #include "FileManager.h"
+#include "GameTimer.h"
 #include "GeometryGenerator.h"
 #include "InputManager.h"
 #include "MathUtils.h"
@@ -33,27 +34,65 @@ std::wstring resourcePath;
 
 void RunApplication()
 {
+	Shader* shader = ResourceManager::Get().CreateResource<Shader>("Shader");
+	shader->Initialize(shaderPath + L"LightEffect.vert", shaderPath + L"LightEffect.frag");
+	
 	std::vector<Vertex> vertices;
 	std::vector<uint32_t> indices;
-	
-	Mesh* cylinder = ResourceManager::Get().CreateResource<Mesh>("Cylinder");
-	GeometryGenerator::CreateCylinder(10.0f, 30.0f, 30, vertices, indices);
-	cylinder->Initialize(vertices, indices);
 
-	Texture2D* metal = ResourceManager::Get().CreateResource<Texture2D>("Metal");
-	metal->Initialize(resourcePath + L"Metal.jpg");
+	Mesh* sphere = ResourceManager::Get().CreateResource<Mesh>("Sphere");
+	GeometryGenerator::CreateSphere(2.0f, 30, vertices, indices);
+	//GeometryGenerator::CreateCylinder(2.0f, 2.0f, 30, vertices, indices);
+	//GeometryGenerator::CreateXZQuad(10.0f, 10.0f, vertices, indices);
+	sphere->Initialize(vertices, indices);
+
+	Vector3f lightPosition;
+	Mesh* light = ResourceManager::Get().CreateResource<Mesh>("Light");
+	GeometryGenerator::CreateSphere(0.5f, 30, vertices, indices);
+	light->Initialize(vertices, indices);
+
+	Texture2D* texture = ResourceManager::Get().CreateResource<Texture2D>("texture");
+	texture->Initialize(resourcePath + L"White.png");
 	
+	GameTimer timer;
+	timer.Reset();
 	while (!bIsDone)
 	{
+		timer.Tick();
 		InputManager::Get().Tick();
 
-		Matrix4x4f view = MathUtils::CreateLookAt(Vector3f(10.0f, 50.0f, 50.0f), Vector3f(0.0f, 0.0f, 0.0f), Vector3f(0.0f, 1.0f, 0.0f));
+		lightPosition = Vector3f(5.0f * MathUtils::Cos(timer.GetTotalSeconds()), 5.0f, 5.0f * MathUtils::Sin(timer.GetTotalSeconds()));
+
+		Matrix4x4f view = MathUtils::CreateLookAt(Vector3f(10.0f, 10.0f, 10.0f), Vector3f(0.0f, 0.0f, 0.0f), Vector3f(0.0f, 1.0f, 0.0f));
 		Matrix4x4f projection = MathUtils::CreatePerspective(MathUtils::ToRadian(45.0f), static_cast<float>(width) / static_cast<float>(height), 0.01f, 1000.0f);
 
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+		RenderManager::Get().RenderAxisGrid3D(view, projection, (-100.0f, -100.0f, -100.0f), Vector3f(+100.0f, +100.0f, +100.0f), 1.0f, Vector4f(1.0f, 1.0f, 1.0f, 0.5f));
 		{
-			RenderManager::Get().RenderAxisGrid3D(view, projection, (-100.0f, -100.0f, -100.0f), Vector3f(+100.0f, +100.0f, +100.0f), 1.0f, Vector4f(1.0f, 1.0f, 1.0f, 0.5f));
-			RenderManager::Get().RenderMesh3D(Matrix4x4f::GetIdentity(), view, projection, cylinder, metal);
+			shader->Bind();
+			shader->SetMatrix4x4fParameter("world", Matrix4x4f::GetIdentity());
+			shader->SetMatrix4x4fParameter("view", view);
+			shader->SetMatrix4x4fParameter("projection", projection);
+			shader->SetVector3fParameter("viewPosition", Vector3f(10.0f, 10.0f, 10.0f));
+			shader->SetVector3fParameter("lightColor", Vector3f(1.0f, 0.5f, 0.31f));
+
+			shader->SetVector3fParameter("material.ambient", Vector3f(0.0215, 0.1745, 0.0215));
+			shader->SetVector3fParameter("material.diffuse", Vector3f(0.07568, 0.61424, 0.07568));
+			shader->SetVector3fParameter("material.specular", Vector3f(0.633, 0.727811, 0.633));
+			shader->SetFloatParameter("material.shininess", 128.0f * 0.6f);
+
+			shader->SetVector3fParameter("light.position", lightPosition);
+			shader->SetVector3fParameter("light.ambient", Vector3f(0.2f, 0.2f, 0.2f));
+			shader->SetVector3fParameter("light.diffuse", Vector3f(0.5f, 0.5f, 0.5f));
+			shader->SetVector3fParameter("light.specular", Vector3f(1.0f, 1.0f, 1.0f));
+
+			glBindVertexArray(sphere->GetVertexArrayObject());
+			glDrawElements(GL_TRIANGLES, sphere->GetIndexCount(), GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
+
+			shader->Unbind();
+
+			RenderManager::Get().RenderMesh3D(MathUtils::CreateTranslation(lightPosition), view, projection, light, texture);
 		}
 		RenderManager::Get().EndFrame();
 	}
