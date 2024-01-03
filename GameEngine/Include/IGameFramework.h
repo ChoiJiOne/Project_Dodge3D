c47fd816b9e522c244b35cc40eb08bc41f251154
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-01-03 14:44:45 +0900

    게임 프레임워크 인터페이스 추가
    - 게임 프레임워크에서 기본적인 초기화/해제를 수행하도록 구현
    - 이 인터페이스를 상속받는 클래스에서는 반드시 Run 메서드 구현하도록 선언

---
+++ GameEngine/Include/IGameFramework.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <array>
+#include <algorithm>
+#include <map>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "Assertion.h"
+#include "CommandLineUtils.h"
+#include "FileManager.h"
+#include "GameTimer.h"
+#include "GeometryGenerator.h"
+#include "GLAssertion.h"
+#include "InputManager.h"
+#include "Macro.h"
+#include "MathUtils.h"
+#include "RenderManager.h"
+#include "ResourceManager.h"
+#include "Shader.h"
+#include "StaticMesh.h"
+#include "StringUtils.h"
+#include "Texture2D.h"
+#include "TTFont.h"
+#include "Window.h"
+#include "WindowsAssertion.h"
+#include "WindowsCrashUtils.h"
+
+
+/**
+ * @brief 게임 프레임워크 인터페이스입니다.
+ * 
+ * @note 게임 루프가 구현되는 클래스는 반드시 이 인터페이스를 상속받아야 합니다.
+ */
+class IGameFramework
+{
+public:
+	/**
+	 * @brief 게임 프레임워크의 디폴트 생성자입니다.
+	 */
+	IGameFramework() = default;
+
+
+	/**
+	 * @brief 게임 프레임워크의 가상 소멸자입니다.
+	 */
+	virtual ~IGameFramework();
+
+
+	/**
+	 * @brief 게임 프레임워크의 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(IGameFramework);
+
+
+	/**
+	 * @brief 게임 프레임워크를 초기화합니다.
+	 */
+	virtual void Setup();
+
+
+	/**
+	 * @brief 게임 프레임워크 사용을 종료합니다.
+	 */
+	virtual void Shutdown();
+
+
+	/**
+	 * @brief 게임 프레임워크를 실행합니다.
+	 */
+	virtual void Run() = 0;
+
+
+protected:
+	/**
+	 * @brief 게임 프레임워크를 초기화했는지 확인합니다.
+	 */
+	bool bIsSetup_ = false;
+
+
+	/**
+	 * @brief 게임 루프 종료가 감지되었는지 확인합니다.
+	 */
+	bool bIsDoneLoop_ = false;
+
+
+	/**
+	 * @brief 루트 경로입니다.
+	 */
+	std::wstring rootPath_;
+
+
+	/**
+	 * @brief 게임 엔진 경로입니다.
+	 */
+	std::wstring enginePath_;
+
+
+	/**
+	 * @brief 게임 윈도우입니다.
+	 */
+	std::unique_ptr<Window> window_ = nullptr;
+};
---
+++ GameEngine/Source/IGameFramework.cpp
@@ -0,0 +1,71 @@
+#include "IGameFramework.h"
+
+IGameFramework::~IGameFramework()
+{
+	if (bIsSetup_)
+	{
+		Shutdown();
+	}
+}
+
+void IGameFramework::Setup()
+{
+	WindowsCrashUtils::RegisterExceptionFilter();
+
+	CommandLineUtils::Parse();
+	ASSERT(CommandLineUtils::GetStringValue(L"rootPath", rootPath_), "failed to get root path from command line...");
+
+	enginePath_ = rootPath_ + L"GameEngine/";
+
+	Window::WindowConstructParam windowParam;
+	ASSERT(CommandLineUtils::GetStringValue(L"title", windowParam.title), "failed to get window title from command line...");
+	ASSERT(CommandLineUtils::GetIntValue(L"x", windowParam.x), "failed to get window x position from command line...");
+	ASSERT(CommandLineUtils::GetIntValue(L"y", windowParam.y), "failed to get window y position from command line...");
+	ASSERT(CommandLineUtils::GetIntValue(L"w", windowParam.w), "failed to get window width size from command line...");
+	ASSERT(CommandLineUtils::GetIntValue(L"h", windowParam.h), "failed to get window height size from command line...");
+	ASSERT(CommandLineUtils::GetBoolValue(L"resize", windowParam.bIsResizable), "failed to get window resize option from command line...");
+	ASSERT(CommandLineUtils::GetBoolValue(L"fullscreen", windowParam.bIsFullscreenMode), "failed to get window fullscreen option from command line...");
+
+	Window::RegisterWindowClass(L"ProjectA", InputManager::WindowProc);
+
+	window_ = std::make_unique<Window>();
+	window_->Create(windowParam);
+
+	InputManager::Get().SetInputControlWindow(window_.get());
+	RenderManager::Get().SetRenderTargetWindow(window_.get());
+
+	FileManager::Get().Startup();
+	InputManager::Get().Startup();
+	ResourceManager::Get().Startup();
+	RenderManager::Get().Startup();
+
+	auto defaultLoopDoneEvent = [&]() { bIsDoneLoop_ = true; };
+	auto defaultResizeEvent = [&]() { RenderManager::Get().Resize(); };
+
+	InputManager::Get().AddWindowEventAction("DefaultLoopDoneEvent", EWindowEvent::Close,         defaultLoopDoneEvent, true);
+	InputManager::Get().AddWindowEventAction("DefaultResizeEvent",   EWindowEvent::Resize,        defaultResizeEvent,   true);
+	InputManager::Get().AddWindowEventAction("DefaultExitMinimize",  EWindowEvent::ExitMinimize,  defaultResizeEvent,   true);
+	InputManager::Get().AddWindowEventAction("DefaultEnterMaximize", EWindowEvent::EnterMaximize, defaultResizeEvent,   true);
+	InputManager::Get().AddWindowEventAction("DefaultExitMaximize",  EWindowEvent::ExitMaximize,  defaultResizeEvent,   true);
+
+	bIsSetup_ = true;
+}
+
+void IGameFramework::Shutdown()
+{
+	if (bIsSetup_)
+	{
+		ResourceManager::Get().Shutdown();
+		RenderManager::Get().Shutdown();
+		InputManager::Get().Shutdown();
+		FileManager::Get().Shutdown();
+
+		window_->Destroy();
+		window_.reset();
+
+		Window::UnregisterWindowClass();
+		WindowsCrashUtils::UnregisterExceptionFilter();
+
+		bIsSetup_ = false;
+	}
+}
