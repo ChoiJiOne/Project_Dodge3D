2d071d2f36e048488a8e1ee60804b202c6cd011a
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-01-04 06:54:25 +0900

    Texture2D의 내부에서 TextureUtils의 기능으로 대체될 수 있는 부분 수정

--- GameEngine/Source/Texture2D.cpp
+++ GameEngine/Source/Texture2D.cpp
@@ -10,7 +10,6 @@
 #include <unordered_map>
 
 #include <glad/glad.h>
-#include <stb_image.h>
 
 Texture2D::~Texture2D()
 {
@@ -59,14 +58,11 @@ void Texture2D::Active(uint32_t unit) const
 
 uint32_t Texture2D::CreateNonCompressionTexture(const std::wstring& path)
 {
-	std::string convertPath = StringUtils::Convert(path);
-
 	int32_t width;
 	int32_t height;
 	int32_t channels;
-
-	uint8_t* buffer = stbi_load(convertPath.c_str(), &width, &height, &channels, 0);
-	ASSERT(buffer != nullptr, "failed to load %s image resource...", convertPath.c_str());
+	std::vector<uint8_t> buffer;
+	TextureUtils::LoadImageFromFile(path, width, height, channels, buffer);
 
 	GLenum format = TextureUtils::FindTextureFormatFromChannel(channels);
 	ASSERT(format != 0xFFFF, "%d is not support image channel...", channels);
@@ -78,13 +74,10 @@ uint32_t Texture2D::CreateNonCompressionTexture(const std::wstring& path)
 	GL_ASSERT(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE), "failed to set texture object warp t...");
 	GL_ASSERT(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR), "failed to set texture object min filter...");
 	GL_ASSERT(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR), "failed to set texture object mag filter...");
-	GL_ASSERT(glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, buffer), "failed to allows elements of an image array to be read by shaders...");
+	GL_ASSERT(glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, buffer.data()), "failed to allows elements of an image array to be read by shaders...");
 	GL_ASSERT(glGenerateMipmap(GL_TEXTURE_2D), "failed to generate texture mipmap...");
 	GL_ASSERT(glBindTexture(GL_TEXTURE_2D, 0), "failed to unbind texture object...");
-
-	stbi_image_free(buffer);
-	buffer = nullptr;
-
+	
 	return textureID;
 }
 
