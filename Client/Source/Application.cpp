3b59530b4e94f5b8661250772678d466812b28c1
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-01-03 14:46:24 +0900

    메인 함수에서는 게임 인터페이스를 초기화/실행/해제만 하도록 변경

--- Client/Source/Application.cpp
+++ Client/Source/Application.cpp
@@ -1,93 +1,10 @@
-#include <array>
-#include <cstdint>
-#include <string>
-#include <windows.h>
-
-#include <glad/glad.h>
-
-#include "Assertion.h"
-#include "CommandLineUtils.h"
-#include "FileManager.h"
-#include "GameTimer.h"
-#include "GeometryGenerator.h"
-#include "InputManager.h"
-#include "MathUtils.h"
-#include "StaticMesh.h"
-#include "RenderManager.h"
-#include "ResourceManager.h"
-#include "Shader.h"
-#include "StringUtils.h"
-#include "Texture2D.h"
-#include "TTFont.h"
-#include "Vector2.h"
-#include "Vector3.h"
-#include "Vector4.h"
-#include "Vertex.h"
-#include "Window.h"
-#include "WindowsCrashUtils.h"
-
-bool bIsDone = false;
-GameTimer globalTimer;
-int32_t width = 800;
-int32_t height = 600;
-std::wstring shaderPath;
-std::wstring resourcePath;
-
-void RunApplication()
-{
-	globalTimer.Reset();
-	while (!bIsDone)
-	{
-		globalTimer.Tick();
-		InputManager::Get().Tick();
-
-		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
-		RenderManager::Get().EndFrame();
-	}
-}
+#include "ClientApplication.h"
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
-	WindowsCrashUtils::RegisterExceptionFilter();
-
-	CommandLineUtils::Parse();
-	CommandLineUtils::GetStringValue(L"shaderPath", shaderPath);
-	CommandLineUtils::GetStringValue(L"resourcePath", resourcePath);
-
-	Window::RegisterWindowClass(L"ProjectA", InputManager::WindowProc);
-
-	Window window;
-	window.Create(L"ProjectA", 200, 200, 800, 600, true, false);
-	InputManager::Get().SetInputControlWindow(&window);
-	RenderManager::Get().SetRenderTargetWindow(&window);
-
-	FileManager::Get().Startup();
-	InputManager::Get().Startup();
-	ResourceManager::Get().Startup();
-	RenderManager::Get().Startup();
-	
-	auto quitEvent = [&]() { bIsDone = true; };
-	auto resizeEvent = [&]() {
-		window.GetSize(width, height);
-		RenderManager::Get().SetViewport(0, 0, width, height);
-		RenderManager::Get().Resize();
-	};
-		
-	InputManager::Get().AddWindowEventAction("CloseLoop",     EWindowEvent::Close,         quitEvent,   true);
-	InputManager::Get().AddWindowEventAction("Resize",        EWindowEvent::Resize,        resizeEvent, true);
-	InputManager::Get().AddWindowEventAction("ExitMinimize",  EWindowEvent::ExitMinimize,  resizeEvent, true);
-	InputManager::Get().AddWindowEventAction("EnterMaximize", EWindowEvent::EnterMaximize, resizeEvent, true);
-	InputManager::Get().AddWindowEventAction("ExitMaximize",  EWindowEvent::ExitMaximize,  resizeEvent, true);
-
-	RunApplication();
-	
-	ResourceManager::Get().Shutdown();
-	RenderManager::Get().Shutdown();
-	InputManager::Get().Shutdown();
-	FileManager::Get().Shutdown();
-	window.Destroy();
-
-	Window::UnregisterWindowClass();
-	WindowsCrashUtils::UnregisterExceptionFilter();
+	std::unique_ptr<IGameFramework> game = std::make_unique<ClientApplication>();
+	game->Setup();
+	game->Run();
+	game->Shutdown();
 	return 0;
 }
