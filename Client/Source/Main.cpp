a7a85eafa0c221690450c9ef7e25a719897a456a
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-01-09 21:00:53 +0900

    임시로 뷰포트 설정을 위한 이벤트 삭제

--- Client/Source/Main.cpp
+++ Client/Source/Main.cpp
@@ -36,18 +36,6 @@ public:
 		IApplication::Setup();
 
 		clientPath_ = rootPath_ + L"Client/";
-
-		auto clientResizeEvent = [&]() {
-			int32_t width;
-			int32_t height;
-			window_->GetSize(width, height);
-			glViewport(0, 0, width, height);
-		};
-
-		InputManager::Get().AddWindowEventAction("ClientResizeEvent", EWindowEvent::Resize, clientResizeEvent, true);
-		InputManager::Get().AddWindowEventAction("ClientExitMinimize", EWindowEvent::ExitMinimize, clientResizeEvent, true);
-		InputManager::Get().AddWindowEventAction("ClientEnterMaximize", EWindowEvent::EnterMaximize, clientResizeEvent, true);
-		InputManager::Get().AddWindowEventAction("ClientExitMaximize", EWindowEvent::ExitMaximize, clientResizeEvent, true);
 	}
 
 
