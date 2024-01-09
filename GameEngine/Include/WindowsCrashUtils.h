fa1a28743379e7cce59f858e03f18d86b28b5e94
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-01-09 20:52:41 +0900

    Windows 크래시 확장 기능을 제공하는 클래스를 네임스페이스로 변경

--- GameEngine/Include/WindowsCrashUtils.h
+++ GameEngine/Include/WindowsCrashUtils.h
@@ -6,61 +6,19 @@
 
 /**
  * @brief Windows 크래시에 대한 확장 기능을 제공합니다.
- * 
- * @note 이 클래스의 모든 멤버 변수와 메서드는 모두 static입니다.
  */
-class WindowsCrashUtils
+namespace WindowsCrashUtils
 {
-public:
 	/**
 	 * @brief 최상위 예외 처리기를 등록합니다.
 	 *
 	 * @see https://learn.microsoft.com/ko-kr/windows/win32/api/errhandlingapi/nf-errhandlingapi-setunhandledexceptionfilter
 	 */
-	static void RegisterExceptionFilter();
+	void RegisterWindowsExceptionFilter();
 
 
 	/**
 	 * @brief 최상위 예외 처리기 등록을 해제합니다.
 	 */
-	static void UnregisterExceptionFilter();
-
-
-	/**
-	 * @brief 크래시를 감지하고 에러 처리를 수행합니다.
-	 * 
-	 * @param exceptionPtr 예외와 컨텍스트 레코드입니다.
-	 * 
-	 * @return EXCEPTION_EXECUTE_HANDLER 값을 반환합니다.
-	 */
-	static LONG WINAPI DetectApplicationCrash(EXCEPTION_POINTERS* exceptionPtr);
-
-
-private:
-	/**
-	 * @brief 크래시가 감지된 시스템 시간값을 얻습니다.
-	 * 
-	 * @return 시스템 시간값을 반환합니다.
-	 */
-	static std::wstring GetCrashSystemTime();
-
-
-	/**
-	 * @brief 크래시 덤프 파일을 생성합니다.
-	 * 
-	 * @param path 크래시 덤프 파일의 경로입니다.
-	 * @param exceptionPtr 예외와 컨텍스트 레코드입니다.
-	 * 
-	 * @return 덤프 파일 생성에 성공했다면 true, 그렇지 않다면 false를 반환합니다.
-	 */
-	static bool CreateMinidumpFile(const std::wstring& path, EXCEPTION_POINTERS* exceptionPtr);
-	
-	
-private:
-	/**
-	 * @brief 크래시와 관련된 파일들을 저장할 경로입니다.
-	 * 
-	 * @note 크래시와 관련된 파일은 미니 덤프 파일과 콜스택 기록 파일입니다.
-	 */
-	static std::wstring crashInfoSavePath_;
+	void UnregisterWindowsExceptionFilter();
 };
--- GameEngine/Source/WindowsCrashUtils.cpp
+++ GameEngine/Source/WindowsCrashUtils.cpp
@@ -1,47 +1,22 @@
 #include "WindowsCrashUtils.h"
 
-#include "StringUtils.h"
-#include "WindowsAssertion.h"
-
 #include <dbghelp.h>
 #include <pathcch.h>
 #include <Shlwapi.h>
 
-std::wstring WindowsCrashUtils::crashInfoSavePath_;
+#include "StringUtils.h"
+#include "WindowsAssertion.h"
+
+std::wstring crashInfoSavePath;
 
 LPTOP_LEVEL_EXCEPTION_FILTER topLevelExceptionFilter;
 
-void WindowsCrashUtils::RegisterExceptionFilter()
-{
-	topLevelExceptionFilter = SetUnhandledExceptionFilter(DetectApplicationCrash);
-
-	wchar_t* bufferPtr = StringUtils::GetWideCharBufferPtr();
-	WINDOWS_ASSERT(GetModuleFileNameW(nullptr, bufferPtr, StringUtils::STRING_BUFFER_SIZE), "failed to get execute file name...");
-	ASSERT(SUCCEEDED(PathCchRemoveFileSpec(bufferPtr, StringUtils::STRING_BUFFER_SIZE)), "failed to remove execute file name...");
-
-	crashInfoSavePath_ = StringUtils::PrintF(L"%s\\Crash\\", bufferPtr);
-	if (!PathFileExistsW(crashInfoSavePath_.c_str()))
-	{
-		WINDOWS_ASSERT(CreateDirectoryW(crashInfoSavePath_.c_str(), nullptr), "failed to create %s directory...", crashInfoSavePath_.c_str());
-	}
-}
-
-void WindowsCrashUtils::UnregisterExceptionFilter()
-{
-	SetUnhandledExceptionFilter(topLevelExceptionFilter);
-}
-
-LONG WINAPI WindowsCrashUtils::DetectApplicationCrash(EXCEPTION_POINTERS* exceptionPtr)
-{
-	std::wstring systemTime = GetCrashSystemTime();
-	std::wstring minidumpPath = StringUtils::PrintF(L"%sWindows-%s-Minidump.dmp", crashInfoSavePath_.c_str(), systemTime.c_str());
-
-	CreateMinidumpFile(minidumpPath, exceptionPtr);
-
-	return EXCEPTION_EXECUTE_HANDLER;
-}
-
-std::wstring WindowsCrashUtils::GetCrashSystemTime()
+/**
+ * @brief 크래시가 감지된 시스템 시간값을 얻습니다.
+ *
+ * @return 시스템 시간값을 반환합니다.
+ */
+std::wstring GetCrashSystemTime()
 {
 	SYSTEMTIME currentSystemTime;
 	GetLocalTime(&currentSystemTime);
@@ -57,7 +32,15 @@ std::wstring WindowsCrashUtils::GetCrashSystemTime()
 	);
 }
 
-bool WindowsCrashUtils::CreateMinidumpFile(const std::wstring& path, EXCEPTION_POINTERS* exceptionPtr)
+/**
+ * @brief 크래시 덤프 파일을 생성합니다.
+ *
+ * @param path 크래시 덤프 파일의 경로입니다.
+ * @param exceptionPtr 예외와 컨텍스트 레코드입니다.
+ *
+ * @return 덤프 파일 생성에 성공했다면 true, 그렇지 않다면 false를 반환합니다.
+ */
+bool CreateMinidumpFile(const std::wstring& path, EXCEPTION_POINTERS* exceptionPtr)
 {
 	HANDLE fileHandle = CreateFileW(path.c_str(), GENERIC_WRITE, FILE_SHARE_WRITE, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
 	if (fileHandle == INVALID_HANDLE_VALUE)
@@ -78,4 +61,41 @@ bool WindowsCrashUtils::CreateMinidumpFile(const std::wstring& path, EXCEPTION_P
 	}
 
 	return bIsSuccessed;
+}
+
+/**
+ * @brief 크래시를 감지하고 에러 처리를 수행합니다.
+ *
+ * @param exceptionPtr 예외와 컨텍스트 레코드입니다.
+ *
+ * @return EXCEPTION_EXECUTE_HANDLER 값을 반환합니다.
+ */
+LONG WINAPI DetectApplicationCrash(EXCEPTION_POINTERS* exceptionPtr)
+{
+	std::wstring systemTime = GetCrashSystemTime();
+	std::wstring minidumpPath = StringUtils::PrintF(L"%sWindows-%s-Minidump.dmp", crashInfoSavePath.c_str(), systemTime.c_str());
+
+	CreateMinidumpFile(minidumpPath, exceptionPtr);
+
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+
+void WindowsCrashUtils::RegisterWindowsExceptionFilter()
+{
+	topLevelExceptionFilter = SetUnhandledExceptionFilter(DetectApplicationCrash);
+
+	wchar_t* bufferPtr = StringUtils::GetWideCharBufferPtr();
+	WINDOWS_ASSERT(GetModuleFileNameW(nullptr, bufferPtr, StringUtils::STRING_BUFFER_SIZE), "failed to get execute file name...");
+	ASSERT(SUCCEEDED(PathCchRemoveFileSpec(bufferPtr, StringUtils::STRING_BUFFER_SIZE)), "failed to remove execute file name...");
+
+	crashInfoSavePath = StringUtils::PrintF(L"%s\\Crash\\", bufferPtr);
+	if (!PathFileExistsW(crashInfoSavePath.c_str()))
+	{
+		WINDOWS_ASSERT(CreateDirectoryW(crashInfoSavePath.c_str(), nullptr), "failed to create %s directory...", crashInfoSavePath.c_str());
+	}
+}
+
+void WindowsCrashUtils::UnregisterWindowsExceptionFilter()
+{
+	SetUnhandledExceptionFilter(topLevelExceptionFilter);
 }
