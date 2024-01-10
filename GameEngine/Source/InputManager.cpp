9195fa374ca2b7451cefab223a79abc644e71282
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-01-11 08:00:01 +0900

    입력 처리 매니저가 IMGUI 이벤트 처리를 수행할 수 있도록 윈도우 이벤트 처리 부분 변경

--- GameEngine/Source/InputManager.cpp
+++ GameEngine/Source/InputManager.cpp
@@ -4,6 +4,9 @@
 #include "Window.h"
 #include "WindowsAssertion.h"
 
+#include <imgui_impl_win32.h>
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
 InputManager* inputManagerPtr = nullptr;
 
 void InputManager::Startup()
@@ -110,6 +113,11 @@ void InputManager::ExecuteWindowEventAction(const EWindowEvent& windowEvent)
 
 LRESULT InputManager::ProcessWindowMessage(HWND windowHandle, uint32_t message, WPARAM wParam, LPARAM lParam)
 {
+	if (ImGui_ImplWin32_WndProcHandler(windowHandle, message, wParam, lParam))
+	{
+		return 1;
+	}
+
 	EWindowEvent windowEvent = EWindowEvent::None;
 
 	switch (message)
