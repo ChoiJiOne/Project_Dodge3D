bac964394a396b774a981e19eb18f9085f171ced
CHOIJIONE\choij <choijione02@gmail.com>
2024-01-11 15:22:05 +0900

    속성 제어 오브젝트의 이름 변경 사항 적용

--- GeometryExporter/Source/Main.cpp
+++ GeometryExporter/Source/Main.cpp
@@ -1,5 +1,8 @@
 #include "IApplication.h"
 
+#include "DirectionalLightShader.h"
+#include "Properties.h"
+
 
 /**
  * @brief Geometry 생성 툴 애플리케이션입니다.
@@ -53,13 +56,63 @@ public:
 	 */
 	virtual void Run() override
 	{
+		Camera3D* camera = ObjectManager::Get().CreateObject<Camera3D>("camera");
+		camera->Initialize(
+			Vector3f(0.0f, 5.0f, 5.0f),
+			Vector3f(0.0f, -1.0f, -1.0f),
+			Vector3f(0.0f, 1.0f, 0.0f),
+			MathUtils::ToRadian(45.0f),
+			window_->GetAspectSize(),
+			0.01f,
+			1000.0f
+		);
+
+		std::vector<Vertex> vertices;
+		std::vector<uint32_t> indices;
+		GeometryGenerator::CreateSphere(1.0f, 40, vertices, indices);
+
+		StaticMesh* sphere = ResourceManager::Get().CreateResource<StaticMesh>("sphere");
+		sphere->Initialize(vertices, indices);
+
+		Material* material = ResourceManager::Get().CreateResource<Material>("material");
+		material->Initialize(
+			Vector3f(0.329412f, 0.223529f, 0.027451f),
+			Vector3f(0.780392f, 0.568627f, 0.113725f),
+			Vector3f(0.992157f, 0.941176f, 0.807843f),
+			128.0f * 0.21794872f
+		);
+
+		DirectionalLightShader* directionalLightShader = ResourceManager::Get().CreateResource<DirectionalLightShader>("DirectionalLightShader");
+		directionalLightShader->Initialize(toolPath_ + L"Shader/DirectionalLight.vert", toolPath_ + L"Shader/DirectionalLight.frag");
+
+		Properties* properties = ObjectManager::Get().CreateObject<Properties>("Properties");
+		properties->Initialize();
+
 		timer_.Reset();
 		while (!bIsDoneLoop_)
 		{
 			timer_.Tick();
 			InputManager::Get().Tick();
-
+			
+			properties->Tick(timer_.GetDeltaSeconds());
+			
+			RenderManager::Get().SetWindowViewport();
+			RenderManager::Get().SetDepthMode(true);
 			RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+			
+			directionalLightShader->Bind();
+			directionalLightShader->SetCamera(camera);
+			directionalLightShader->SetMaterial(material);
+			directionalLightShader->SetDirectionalLight(properties->GetDirectionalLight());
+
+			directionalLightShader->DrawMesh3D(Matrix4x4f::GetIdentity(), sphere);
+
+			directionalLightShader->Unbind();
+
+			RenderManager::Get().RenderGrid3D(
+				camera->GetViewMatrix(), camera->GetProjectionMatrix(),
+				-10.0f, +10.0f, 1.0f, -10.0f, +10.0f, 1.0f, Vector4f(1.0f, 1.0f, 1.0f, 0.5f)
+			);
 			RenderManager::Get().EndFrame();
 		}
 	}
