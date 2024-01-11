bfcf560c8e31d72951dbf2b9502f4faf9357aac8
CHOIJIONE\choij <choijione02@gmail.com>
2024-01-11 12:34:05 +0900

    ImGui 테스트용 코드 삭제

--- Client/Source/Main.cpp
+++ Client/Source/Main.cpp
@@ -68,25 +68,8 @@ public:
 		{
 			timer_.Tick();
 			InputManager::Get().Tick();
-			
-			static float f = 0.0f;
-			static int counter = 0;
 
-			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-			if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-				counter++;
-			ImGui::SameLine();
-			ImGui::Text("counter = %d", counter);
-
-			ImGui::End();
-
-			RenderManager::Get().BeginFrame(clear_color.x, clear_color.y, clear_color.z, 1.0f);
-			RenderManager::Get().EndFrame();
+			demoScene_->Tick(timer_.GetDeltaSeconds());
 		}
 	}
 
