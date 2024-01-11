5257f6c30303d70b4035909d6fdba7e53ef7d3da
CHOIJIONE\choij <choijione02@gmail.com>
2024-01-11 15:02:54 +0900

    제어 가능한 라이트 프로퍼티 오브젝트 추가

---
+++ GeometryExporter/Source/LightProperties.cpp
@@ -0,0 +1,51 @@
+#include "LightProperties.h"
+
+#include "Assertion.h"
+
+#include <imgui.h>
+
+LightProperties::~LightProperties()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void LightProperties::Initialize()
+{
+	ASSERT(!bIsInitialized_, "already initialize light properties object...");
+
+	direction_ = Vector3f(-1.0f, -1.0f, -1.0f);
+	ambientRGB_ = Vector3f(+0.5f, +0.5f, +0.5f);
+	diffuseRGB_ = Vector3f(+0.7f, +0.7f, +0.7f);
+	specularRGB_ = Vector3f(+1.0f, +1.0f, +1.0f);
+
+	directionalLight_ = DirectionalLight(direction_, ambientRGB_, diffuseRGB_, specularRGB_);
+	
+	bIsInitialized_ = true;
+}
+
+void LightProperties::Tick(float deltaSeconds)
+{
+	ImGui::Begin("Light Properties");
+
+	ImGui::SliderFloat3("direction", direction_.GetPtr(), -1.0f, 1.0f);
+	ImGui::ColorEdit3("ambient", ambientRGB_.GetPtr());
+	ImGui::ColorEdit3("diffuse", diffuseRGB_.GetPtr());
+	ImGui::ColorEdit3("specular", specularRGB_.GetPtr());
+
+	directionalLight_.SetDirection(direction_);
+	directionalLight_.SetAmbientRGB(ambientRGB_);
+	directionalLight_.SetDiffuseRGB(diffuseRGB_);
+	directionalLight_.SetSpecularRGB(specularRGB_);
+
+	ImGui::End();
+}
+
+void LightProperties::Release()
+{
+	ASSERT(bIsInitialized_, "not initialized before or has already been released...");
+
+	bIsInitialized_ = false;
+}
