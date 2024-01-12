2717cd1d4a7a9061ee718f6653b93e520fde7f7b
CHOIJIONE\choij <choijione02@gmail.com>
2024-01-12 20:14:47 +0900

    사운드 리소스의 기본적인 제어 기능 추가

--- GameEngine/Include/Sound.h
+++ GameEngine/Include/Sound.h
@@ -50,6 +50,84 @@ public:
 	virtual void Release() override;
 
 
+	/**
+	 * @brief 사운드의 크기를 설정합니다.
+	 *
+	 * @param volume 사운드의 크기입니다. 범위는 0.0 ~ 1.0 입니다.
+	 */
+	void SetVolume(float volume);
+
+
+	/**
+	 * @brief 현재 사운드의 크기를 얻습니다.
+	 *
+	 * @note 사운드 크기의 범위는 0.0 ~ 1.0 입니다.
+	 *
+	 * @return 현재 사운드의 크기를 얻습니다.
+	 */
+	float GetVolume();
+
+
+	/**
+	 * @brief 사운드의 반복 여부를 설정합니다.
+	 *
+	 * @param bIsLoop 사운드 반복 여부입니다.
+	 */
+	void SetLooping(bool bIsLoop);
+
+
+	/**
+	 * @brief 사운드의 반복 여부를 얻습니다.
+	 *
+	 * @return 사운드가 반복한다면 true, 그렇지 않다면 false를 반환합니다.
+	 */
+	bool IsLooping();
+
+
+	/**
+	 * @brief 사운드를 플레이합니다.
+	 *
+	 * @note 이전에 중지한 적이 있다면 중지한 시점부터 플레이됩니다.
+	 */
+	void Play();
+
+
+	/**
+	 * @brief 사운드가 플레이중인지 확인합니다.
+	 *
+	 * @return 사운드가 플레이 중이라면 true, 그렇지 않다면 false를 반환합니다.
+	 */
+	bool IsPlaying();
+
+
+	/**
+	 * @brief 사운드 플레이가 끝났는지 확인합니다.
+	 *
+	 * @return 사운드 플레이가 끝났다면 true, 그렇지 않다면 false를 반환합니다.
+	 */
+	bool IsDone();
+
+
+	/**
+	 * @brief 사운드 플레이를 중지합니다.
+	 */
+	void Stop();
+
+
+	/**
+	 * @brief 사운드 플레이가 중지 되었는지 확인합니다.
+	 *
+	 * @return 사운드 플레이가 중지 되었다면 true, 그렇지 않으면 false를 반환합니다.
+	 */
+	bool IsStopping();
+
+
+	/**
+	 * @brief 사운드를 초기화합니다.
+	 */
+	void Reset();
+
+
 private:
 	/**
 	 * @brief miniaudio 사운드 리소스입니다.
--- GameEngine/Source/Sound.cpp
+++ GameEngine/Source/Sound.cpp
@@ -2,6 +2,7 @@
 
 #include "Assertion.h"
 #include "AudioManager.h"
+#include "MathUtils.h"
 
 Sound::~Sound()
 {
@@ -29,4 +30,55 @@ void Sound::Release()
 	sound_.reset();
 
 	bIsInitialized_ = false;
+}
+
+void Sound::SetVolume(float volume)
+{
+	volume = MathUtils::Clamp<float>(volume, 0.0f, 1.0f);
+	ma_sound_set_volume(sound_.get(), volume);
+}
+
+float Sound::GetVolume()
+{
+	return ma_sound_get_volume(sound_.get());
+}
+
+void Sound::SetLooping(bool bIsLoop)
+{
+	ma_sound_set_looping(sound_.get(), static_cast<ma_bool32>(bIsLoop));
+}
+
+bool Sound::IsLooping()
+{
+	return ma_sound_is_looping(sound_.get()) == MA_TRUE;
+}
+
+void Sound::Play()
+{
+	ma_sound_start(sound_.get());
+}
+
+bool Sound::IsPlaying()
+{
+	return ma_sound_is_playing(sound_.get()) == MA_TRUE;
+}
+
+bool Sound::IsDone()
+{
+	return ma_sound_at_end(sound_.get()) == MA_TRUE;
+}
+
+void Sound::Stop()
+{
+	ma_sound_stop(sound_.get());
+}
+
+bool Sound::IsStopping()
+{
+	return !IsDone() && !IsPlaying();
+}
+
+void Sound::Reset()
+{
+	ma_sound_seek_to_pcm_frame(sound_.get(), 0);
 }
