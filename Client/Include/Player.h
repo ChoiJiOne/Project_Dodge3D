e57e4b7b8380174d5b49e4bd1e965e49e3687258
CHOIJIONE\choij <choijione02@gmail.com>
2024-01-15 18:27:35 +0900

    플레이어가 조종 가능한 오브젝트 추가
    - 현재 플레이어가 조종할 수 있는 기능은 미구현된 상태
    - 초기화만 수행하도록 구현

---
+++ Client/Include/Player.h
@@ -0,0 +1,109 @@
+#pragma once
+
+#include "Sphere3D.h"
+#include "IObject.h"
+
+class Material;
+class StaticMesh;
+
+
+/**
+ * @brief 플레이어가 조종 가능한 오브젝트입니다.
+ */
+class Player : public IObject
+{
+public:
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트의 기본 생성자입니다.
+	 *
+	 * @note 생성자 이외의 메서드에서 적절한 초기화를 수행해야 합니다.
+	 */
+	Player() = default;
+
+
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트의 가상 소멸자입니다.
+	 */
+	virtual ~Player();
+
+
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트의 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(Player);
+
+
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트를 초기화합니다.
+	 */
+	void Initialize();
+
+
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트를 업데이트합니다.
+	 */
+	virtual void Tick(float deltaSeconds) override;
+
+
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트 내의 리소스를 할당 해제합니다.
+	 */
+	virtual void Release() override;
+
+
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트의 메시를 얻습니다.
+	 *
+	 * @return 플레이어가 조종 가능한 오브젝트의 메시를 반환합니다.
+	 */
+	StaticMesh* GetMesh() const { return mesh_; }
+
+
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트의 재질을 얻습니다.
+	 *
+	 * @return 플레이어가 조종 가능한 오브젝트의 재질을 반환합니다.
+	 */
+	Material* GetMaterial() const { return material_; }
+
+
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트의 현재 위치를 얻습니다.
+	 *
+	 * @return 플레이어가 조종 가능한 오브젝트의 위치를 반환합니다.
+	 */
+	const Vector3f& GetPosition() const { return position_; }
+
+
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트의 경계 영역을 얻습니다.
+	 *
+	 * @return 플레이어가 조종 가능한 오브젝트의 경계 영역을 반환합니다.
+	 */
+	const Sphere3D& GetBoundingVolume() const { return boundingVolume_; }
+
+
+private:
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트의 정적 메시입니다.
+	 */
+	StaticMesh* mesh_ = nullptr;
+
+
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트의 재질입니다.
+	 */
+	Material* material_ = nullptr;
+
+
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트의 현재 위치입니다.
+	 */
+	Vector3f position_;
+
+
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트의 경계 영역입니다.
+	 */
+	Sphere3D boundingVolume_;
+};
---
+++ Client/Source/Player.cpp
@@ -0,0 +1,62 @@
+#include "Player.h"
+
+#include "Assertion.h"
+#include "GeometryGenerator.h"
+#include "Material.h"
+#include "ResourceManager.h"
+#include "StaticMesh.h"
+
+Player::~Player()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void Player::Initialize()
+{
+	ASSERT(!bIsInitialized_, "already initialize player object...");
+
+	mesh_ = ResourceManager::Get().GetResource<StaticMesh>("PlayerMesh");
+	if (!mesh_)
+	{
+		std::vector<Vertex> vertices;
+		std::vector<uint32_t> indices;
+		GeometryGenerator::CreateCone(0.5f, 1.0f, 40, vertices, indices);
+		
+		mesh_ = ResourceManager::Get().CreateResource<StaticMesh>("PlayerMesh");
+		mesh_->Initialize(vertices, indices);
+	}
+
+	material_ = ResourceManager::Get().GetResource<Material>("PlayerMaterial");
+	if (!material_)
+	{
+		material_ = ResourceManager::Get().CreateResource<Material>("PlayerMaterial");
+		material_->Initialize(
+			Vector3f(0.2125f, 0.1275f, 0.054f),
+			Vector3f(0.714f, 0.4284f, 0.18144f),
+			Vector3f(0.393548f, 0.271906f, 0.166721f),
+			0.2f * 128.0f
+		);
+	}
+
+	position_ = Vector3f(0.0f, 0.5f, 0.0f);
+	boundingVolume_ = Sphere3D(position_, 0.5f);
+
+	bIsInitialized_ = true;
+}
+
+void Player::Tick(float deltaSeconds)
+{
+	
+
+
+}
+
+void Player::Release()
+{
+	ASSERT(bIsInitialized_, "not initialized before or has already been released...");
+
+	bIsInitialized_ = false;
+}
