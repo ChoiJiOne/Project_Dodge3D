e6ad39f66d2d9da090ec773c2ccd02e063a9c106
CHOIJIONE\choij <choijione02@gmail.com>
2024-01-15 19:18:47 +0900

    플레이어가 바라보는 방향 기준으로 동쪽 벽 오브젝트 추가

---
+++ Client/Source/EastWall.cpp
@@ -0,0 +1,59 @@
+#include "EastWall.h"
+
+#include "Assertion.h"
+#include "GeometryGenerator.h"
+#include "Material.h"
+#include "ResourceManager.h"
+#include "StaticMesh.h"
+
+EastWall::~EastWall()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void EastWall::Initialize()
+{
+	ASSERT(!bIsInitialized_, "already initialize east wall object...");
+
+	mesh_ = ResourceManager::Get().GetResource<StaticMesh>("EastWallMesh");
+	if (!mesh_)
+	{
+		std::vector<Vertex> vertices;
+		std::vector<uint32_t> indices;
+		GeometryGenerator::CreateCube(Vector3f(1.0f, 1.0f, 10.0f), vertices, indices);
+
+		mesh_ = ResourceManager::Get().CreateResource<StaticMesh>("EastWallMesh");
+		mesh_->Initialize(vertices, indices);
+	}
+
+	material_ = ResourceManager::Get().GetResource<Material>("EastWallMaterial");
+	if (!material_)
+	{
+		material_ = ResourceManager::Get().CreateResource<Material>("EastWallMaterial");
+		material_->Initialize(
+			Vector3f(0.19225f, 0.19225f, 0.19225f),
+			Vector3f(0.50754f, 0.50754f, 0.50754f),
+			Vector3f(0.508273f, 0.508273f, 0.508273f),
+			0.4f * 128.0f
+		);
+	}
+
+	position_ = Vector3f(+5.5f, 0.5f, 0.0f);
+	boundingVolume_ = Box3D(position_, Vector3f(1.0f, 1.0f, 10.0f));
+
+	bIsInitialized_ = true;
+}
+
+void EastWall::Tick(float deltaSeconds)
+{
+}
+
+void EastWall::Release()
+{
+	ASSERT(bIsInitialized_, "not initialized before or has already been released...");
+
+	bIsInitialized_ = false;
+}
