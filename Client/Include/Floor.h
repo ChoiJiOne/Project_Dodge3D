8ce7de6046e62c063c8c0c139a04f794a348c851
CHOIJIONE\choij <choijione02@gmail.com>
2024-01-15 18:12:33 +0900

    게임 내의 바닥 오브젝트 추가

---
+++ Client/Include/Floor.h
@@ -0,0 +1,109 @@
+#pragma once
+
+#include "Box3D.h"
+#include "IObject.h"
+
+class Material;
+class StaticMesh;
+
+
+/**
+ * @brief 바닥 오브젝트입니다.
+ */
+class Floor : public IObject
+{
+public:
+	/**
+	 * @brief 바닥 오브젝트의 기본 생성자입니다.
+	 * 
+	 * @note 생성자 이외의 메서드에서 적절한 초기화를 수행해야 합니다.
+	 */
+	Floor() = default;
+
+
+	/**
+	 * @brief 바닥 오브젝트의 가상 소멸자입니다.
+	 */
+	virtual ~Floor();
+
+
+	/**
+	 * @brief 바닥 오브젝트의 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(Floor);
+
+
+	/**
+	 * @brief 바닥 오브젝트를 초기화합니다.
+	 */
+	void Initialize();
+
+
+	/**
+	 * @brief 바닥 오브젝트를 업데이트합니다.
+	 */
+	virtual void Tick(float deltaSeconds) override;
+
+
+	/**
+	 * @brief 바닥 오브젝트 내의 리소스를 할당 해제합니다.
+	 */
+	virtual void Release() override;
+
+
+	/**
+	 * @brief 바닥 오브젝트의 메시를 얻습니다.
+	 * 
+	 * @return 바닥 오브젝트의 메시를 반환합니다.
+	 */
+	StaticMesh* GetMesh() const { return mesh_; }
+
+
+	/**
+	 * @brief 바닥 오브젝트의 재질을 얻습니다.
+	 * 
+	 * @return 바닥 오브젝트의 재질을 반환합니다.
+	 */
+	Material* GetMaterial() const { return material_; }
+
+
+	/**
+	 * @brief 바닥 오브젝트의 현재 위치를 얻습니다.
+	 * 
+	 * @return 바닥 오브젝트의 위치를 반환합니다.
+	 */
+	const Vector3f& GetPosition() const { return position_; }
+
+
+	/**
+	 * @brief 바닥 오브젝트의 경계 영역을 얻습니다.
+	 * 
+	 * @return 바닥 오브젝트의 경계 영역을 반환합니다.
+	 */
+	const Box3D& GetBoundingVolume() const { return boundingVolume_; }
+
+
+private:
+	/**
+	 * @brief 바닥 오브젝트의 정적 메시입니다.
+	 */
+	StaticMesh* mesh_ = nullptr;
+
+
+	/**
+	 * @brief 바닥 오브젝트의 재질입니다.
+	 */
+	Material* material_ = nullptr;
+
+
+	/**
+	 * @brief 바닥 오브젝트의 현재 위치입니다.
+	 */
+	Vector3f position_;
+
+
+	/**
+	 * @brief 바닥 오브젝트의 경계 영역입니다.
+	 */
+	Box3D boundingVolume_;
+};
---
+++ Client/Source/Floor.cpp
@@ -0,0 +1,59 @@
+#include "Floor.h"
+
+#include "Assertion.h"
+#include "GeometryGenerator.h"
+#include "Material.h"
+#include "ResourceManager.h"
+#include "StaticMesh.h"
+
+Floor::~Floor()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void Floor::Initialize()
+{
+	ASSERT(!bIsInitialized_, "already initialize floor object...");
+
+	mesh_ = ResourceManager::Get().GetResource<StaticMesh>("FloorMesh");
+	if (!mesh_)
+	{
+		std::vector<Vertex> vertices;
+		std::vector<uint32_t> indices;
+		GeometryGenerator::CreateCube(Vector3f(10.0f, 1.0f, 10.0f), vertices, indices);
+
+		mesh_ = ResourceManager::Get().CreateResource<StaticMesh>("FloorMesh");
+		mesh_->Initialize(vertices, indices);
+	}
+
+	material_ = ResourceManager::Get().GetResource<Material>("FloorMaterial");
+	if (!material_)
+	{
+		material_ = ResourceManager::Get().CreateResource<Material>("FloorMaterial");
+		material_->Initialize(
+			Vector3f(0.19125f, 0.0735f, 0.0225f),
+			Vector3f(0.7038f, 0.27048f, 0.0828f),
+			Vector3f(0.256777f, 0.137622f, 0.086014f),
+			0.1f * 128.0f
+		);
+	}
+
+	position_ = Vector3f(0.0f, -0.5f, 0.0f);
+	boundingVolume_ = Box3D(position_, Vector3f(10.0f, 1.0f, 10.0f));
+
+	bIsInitialized_ = true;
+}
+
+void Floor::Tick(float deltaSeconds)
+{
+}
+
+void Floor::Release()
+{
+	ASSERT(bIsInitialized_, "not initialized before or has already been released...");
+
+	bIsInitialized_ = false;
+}
