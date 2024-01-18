e753c612a90772a4935f272bf4f6558fdef16741
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-01-18 10:41:49 +0900

    게임 씬에 총알 생성 오브젝트 추가 및 씬 초기화 부분을 메서드로 분리

--- Client/Include/GameScene.h
+++ Client/Include/GameScene.h
@@ -1,13 +1,24 @@
 #pragma once
 
+#include <array>
+
 #include "GameObject.h"
 #include "IScene.h"
 #include "LightShader.h"
+#include "Skybox.h"
 #include "ShadowMap.h"
 #include "ShadowShader.h"
 
+#include "Bullet.h"
+#include "BulletSpawner.h"
+#include "EastWall.h"
+#include "Floor.h"
 #include "MovableCamera.h"
+#include "NorthWall.h"
+#include "Player.h"
+#include "SouthWall.h"
 #include "StaticLight.h"
+#include "WestWall.h"
 
 
 /**
@@ -44,29 +55,18 @@ public:
 
 private:
 	/**
-	 * @brief 플레이어가 조종하는 오브젝트를 따라다니는 카메라입니다.
+	 * @brief 게임 씬의 리소스를 로딩합니다.
 	 */
-	MovableCamera* camera_ = nullptr;
+	void LoadResources();
 
 
 	/**
-	 * @brief 게임 내의 정적 라이트입니다.
+	 * @brief 게임 씬의 오브젝트를 로딩합니다.
 	 */
-	StaticLight* light_ = nullptr;
+	void LoadObjects();
+	
 
-
-	/**
-	 * @brief 게임 내의 업데이트 가능한 오브젝트들입니다.
-	 */
-	std::vector<GameObject*> updateObjects_;
-
-
-	/**
-	 * @brief 게임 내의 렌더링 가능한 오브젝트들입니다.
-	 */
-	std::vector<GameObject*> renderObjects_;
-
-
+private:
 	/**
 	 * @brief 게임 내 렌더링 씬의 깊이 값을 기록한 텍스처입니다.
 	 */
@@ -90,8 +90,93 @@ private:
 	 */
 	ShadowShader* shadowShader_ = nullptr;
 
+
 	/**
 	 * @brief 게임 내 렌더링 씬에 라이팅 효과를 적용하는 셰이더입니다.
 	 */
 	LightShader* lightShader_ = nullptr;
+
+
+	/**
+	 * @brief 플레이어가 조종하는 오브젝트를 따라다니는 카메라입니다.
+	 */
+	MovableCamera* camera_ = nullptr;
+
+
+	/**
+	 * @brief 게임 내의 정적 라이트입니다.
+	 */
+	StaticLight* light_ = nullptr;
+
+
+	/**
+	 * @brief 플레이어가 조종 가능한 오브젝트입니다.
+	 */
+	Player* player_ = nullptr;
+
+
+	/**
+	 * @brief 바닥 오브젝트입니다.
+	 */
+	Floor* floor_ = nullptr;
+
+
+	/**
+	 * @brief 플레이어가 바라보는 방향 기준의 북쪽 벽입니다.
+	 */
+	NorthWall* northWall_ = nullptr;
+
+	
+	/**
+	 * @brief 플레이어가 바라보는 방향 기준의 남쪽 벽입니다.
+	 */
+	SouthWall* southWall_ = nullptr;
+
+
+	/**
+	 * @brief 플레이어가 바라보는 방향 기준의 서쪽 벽입니다.
+	 */
+	WestWall* westWall_ = nullptr;
+
+
+	/**
+	 * @brief 플레이어가 바라보는 방향 기준의 동쪽 벽입니다.
+	 */
+	EastWall* eastWall_ = nullptr;
+
+
+	/**
+	 * @brief 플레이어가 피해야 할 총알을 생성하는 오브젝트입니다.
+	 */
+	BulletSpawner* bulletSpawner0_ = nullptr;
+
+
+	/**
+	 * @brief 플레이어가 피해야 할 총알을 생성하는 오브젝트입니다.
+	 */
+	BulletSpawner* bulletSpawner1_ = nullptr;
+
+
+	/**
+	 * @brief 플레이어가 피해야 할 총알을 생성하는 오브젝트입니다.
+	 */
+	BulletSpawner* bulletSpawner2_ = nullptr;
+
+
+	/**
+	 * @brief 플레이어가 피해야 할 총알을 생성하는 오브젝트입니다.
+	 */
+	BulletSpawner* bulletSpawner3_ = nullptr;
+
+
+	/**
+	 * @brief 게임 내의 업데이트 가능한 오브젝트들입니다.
+	 */
+	std::array<GameObject*, 10> updateObjects_;
+
+
+	/**
+	 * @brief 게임 내의 렌더링 가능한 오브젝트들입니다.
+	 */
+	std::array<GameObject*, 10> renderObjects_;
 };
--- Client/Source/GameScene.cpp
+++ Client/Source/GameScene.cpp
@@ -17,69 +17,11 @@
 
 GameScene::GameScene()
 {
-	camera_ = ObjectManager::Get().CreateObject<MovableCamera>("MainCamera");
-	camera_->Initialize();
-
-	light_ = ObjectManager::Get().CreateObject<StaticLight>("GlobalLight");
-	light_->Initialize();
-
-	Player* player = ObjectManager::Get().CreateObject<Player>("Player");
-	player->Initialize();
-	updateObjects_.push_back(player);
-	renderObjects_.push_back(player);
-
-	Floor* floor = ObjectManager::Get().CreateObject<Floor>("Floor");
-	floor->Initialize();
-	updateObjects_.push_back(floor);
-	renderObjects_.push_back(floor);
+	LoadResources();
+	LoadObjects();
 	
-	NorthWall* northWall = ObjectManager::Get().CreateObject<NorthWall>("NorthWall");
-	northWall->Initialize();
-	updateObjects_.push_back(northWall);
-	renderObjects_.push_back(northWall);
-
-	SouthWall* southWall = ObjectManager::Get().CreateObject<SouthWall>("SouthWall");
-	southWall->Initialize();
-	updateObjects_.push_back(southWall);
-	renderObjects_.push_back(southWall);
-
-	WestWall* westWall = ObjectManager::Get().CreateObject<WestWall>("WestWall");
-	westWall->Initialize();
-	updateObjects_.push_back(westWall);
-	renderObjects_.push_back(westWall);
-
-	EastWall* eastWall = ObjectManager::Get().CreateObject<EastWall>("EastWall");
-	eastWall->Initialize();
-	updateObjects_.push_back(eastWall);
-	renderObjects_.push_back(eastWall);
-	
-	BulletSpawner* bulletSpawner0 = ObjectManager::Get().CreateObject<BulletSpawner>(StringUtils::PrintF("BulletSpawner_%d", BulletSpawner::GetCountOfBulletSpawner()));
-	bulletSpawner0->Initialize(Vector3f(-4.0f, 0.5f, +4.0f));
-	updateObjects_.push_back(bulletSpawner0);
-	renderObjects_.push_back(bulletSpawner0);
-
-	BulletSpawner* bulletSpawner1 = ObjectManager::Get().CreateObject<BulletSpawner>(StringUtils::PrintF("BulletSpawner_%d", BulletSpawner::GetCountOfBulletSpawner()));
-	bulletSpawner1->Initialize(Vector3f(+4.0f, 0.5f, +4.0f));
-	updateObjects_.push_back(bulletSpawner1);
-	renderObjects_.push_back(bulletSpawner1);
-
-	BulletSpawner* bulletSpawner2 = ObjectManager::Get().CreateObject<BulletSpawner>(StringUtils::PrintF("BulletSpawner_%d", BulletSpawner::GetCountOfBulletSpawner()));
-	bulletSpawner2->Initialize(Vector3f(+4.0f, 0.5f, -4.0f));
-	updateObjects_.push_back(bulletSpawner2);
-	renderObjects_.push_back(bulletSpawner2);
-
-	BulletSpawner* bulletSpawner3 = ObjectManager::Get().CreateObject<BulletSpawner>(StringUtils::PrintF("BulletSpawner_%d", BulletSpawner::GetCountOfBulletSpawner()));
-	bulletSpawner3->Initialize(Vector3f(-4.0f, 0.5f, -4.0f));
-	updateObjects_.push_back(bulletSpawner3);
-	renderObjects_.push_back(bulletSpawner3);
-
-	shadowMap_ = ResourceManager::Get().CreateResource<ShadowMap>("shadowMap");
-	shadowMap_->Initialize(SHADOW_WIDTH, SHADOW_HEIGHT);
-
-	shadowShader_ = ResourceManager::Get().GetResource<ShadowShader>("ShadowShader");
-	lightShader_ = ResourceManager::Get().GetResource<LightShader>("LightShader");
-
-	auto gameSceneResizeEvent = [&]() {
+	auto gameSceneResizeEvent = [&]() 
+	{
 		camera_->SetAspectRatio(RenderManager::Get().GetRenderTargetWindow()->GetAspectSize());
 	};
 
@@ -137,4 +79,82 @@ void GameScene::Tick(float deltaSeconds)
 		lightShader_->Unbind();
 	}
 	RenderManager::Get().EndFrame();
+}
+
+void GameScene::LoadResources()
+{
+	shadowMap_ = ResourceManager::Get().GetResource<ShadowMap>("ShadowMap");
+	if(!shadowMap_)
+	{
+		shadowMap_ = ResourceManager::Get().CreateResource<ShadowMap>("ShadowMap");
+		shadowMap_->Initialize(SHADOW_WIDTH, SHADOW_HEIGHT);
+	}
+
+	shadowShader_ = ResourceManager::Get().GetResource<ShadowShader>("ShadowShader");
+	lightShader_ = ResourceManager::Get().GetResource<LightShader>("LightShader");
+}
+
+void GameScene::LoadObjects()
+{
+	camera_ = ObjectManager::Get().CreateObject<MovableCamera>("MainCamera");
+	camera_->Initialize();
+
+	light_ = ObjectManager::Get().CreateObject<StaticLight>("GlobalLight");
+	light_->Initialize();
+
+	player_ = ObjectManager::Get().CreateObject<Player>("Player");
+	player_->Initialize();
+
+	floor_ = ObjectManager::Get().CreateObject<Floor>("Floor");
+	floor_->Initialize();
+
+	northWall_ = ObjectManager::Get().CreateObject<NorthWall>("NorthWall");
+	northWall_->Initialize();
+
+	southWall_ = ObjectManager::Get().CreateObject<SouthWall>("SouthWall");
+	southWall_->Initialize();
+
+	westWall_ = ObjectManager::Get().CreateObject<WestWall>("WestWall");
+	westWall_->Initialize();
+
+	eastWall_ = ObjectManager::Get().CreateObject<EastWall>("EastWall");
+	eastWall_->Initialize();
+
+	bulletSpawner0_ = ObjectManager::Get().CreateObject<BulletSpawner>("BulletSpawner_0");
+	bulletSpawner0_->Initialize(Vector3f(-4.0f, 0.5f, +4.0f), 1.0f);
+
+	bulletSpawner1_ = ObjectManager::Get().CreateObject<BulletSpawner>("BulletSpawner_1");
+	bulletSpawner1_->Initialize(Vector3f(+4.0f, 0.5f, +4.0f), 2.0f);
+
+	bulletSpawner2_ = ObjectManager::Get().CreateObject<BulletSpawner>("BulletSpawner_2");
+	bulletSpawner2_->Initialize(Vector3f(+4.0f, 0.5f, -4.0f), 1.0f);
+
+	bulletSpawner3_ = ObjectManager::Get().CreateObject<BulletSpawner>("BulletSpawner_3");
+	bulletSpawner3_->Initialize(Vector3f(-4.0f, 0.5f, -4.0f), 2.0f);
+
+	updateObjects_ = {
+		player_,
+		bulletSpawner0_,
+		bulletSpawner1_,
+		bulletSpawner2_,
+		bulletSpawner3_,
+		floor_,
+		northWall_,
+		southWall_,
+		westWall_,
+		eastWall_,
+	};
+
+	renderObjects_ = {
+		floor_,
+		northWall_,
+		southWall_,
+		westWall_,
+		eastWall_,
+		bulletSpawner0_,
+		bulletSpawner1_,
+		bulletSpawner2_,
+		bulletSpawner3_,
+		player_,
+	};
 }
