19061ca6fc05fc53638bab42babbac3fe1410d55
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-12-11 20:26:49 +0900

    게임 엔진 내에서 사용하던 스크립트 추가

---
+++ Script/Package.bat
@@ -0,0 +1,38 @@
+@echo off
+
+echo Start "Package.bat" script...
+
+set solutionPath=%~dp0..\..\Solution
+
+@REM 프로젝트가 존재하지 않으면 프로젝트를 생성합니다.
+if not exist "%solutionPath%" (
+    echo Can't find Visual Studio Solution...
+    GOTO:EOF
+)
+
+@REM 패키징할 모드입니다.
+SET mode=%1
+
+@REM 패키징할 모드의 유효성을 검사합니다.
+if "%mode%" == "Debug" (
+    echo Build "%mode%" mode...
+) else if "%mode%" == "Release" (
+    echo Build "%mode%" mode...
+) else if "%mode%" == "RelWithDebInfo" (
+    echo Build "%mode%" mode...
+) else if "%mode%" == "MinSizeRel" (
+    echo Build "%mode%" mode...
+) else (
+    echo "%mode%" is illegal mode...
+    GOTO:EOF
+)
+
+set currentPath=%~dp0
+
+pushd "%currentPath%"
+pushd "%solutionPath%"
+
+cpack -C %mode% -G NSIS
+
+popd
+popd
