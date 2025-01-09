0356937cda9f2de03b42b49ebf83756003af2e38
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-10 07:41:45 +0900

    아무 동작도 수행하지 않는 메인 함수 추가

---
+++ DodgeBall/Src/Main.cpp
@@ -0,0 +1,11 @@
+#include <cstdint>
+#include <Windows.h>
+
+#if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
+#include <crtdbg.h>
+#endif
+
+int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
+{
+	return 0;
+}
