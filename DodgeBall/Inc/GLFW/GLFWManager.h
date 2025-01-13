6def75f4c12ee04cc4111016a1e62b9b77e34767
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-14 08:35:49 +0900

    이전 프로젝트에서 사용하던 GLFW 매니저를 현재 프로젝트에 맞게 변경 후 추가

--- DodgeBall/Inc/GLFW/GLFWManager.h
+++ DodgeBall/Inc/GLFW/GLFWManager.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <array>
+#include <functional>
+#include <string>
+
 #include <glfw/glfw3.h>
+#include <glm/glm.hpp>
 
 #include "Utils/Macro.h"
 
@@ -184,6 +189,42 @@ public:
 	/** GLFW 매니저의 싱글턴 객체 포인터를 얻습니다. */
 	static GLFWManager* GetPtr();
 
+	/** GLFW 매니저의 초기화를 수행합니다. */
+	void Startup(int32_t width, int32_t height, const char* title, bool bIsWindowCentered);
+
+	/** GLFW 매니저의 초기화 해제를 수행합니다. */
+	void Shutdown();
+
+	/** 한 프레임을 시작합니다. */
+	void Tick();
+
+	/** 현재 윈도우 크기를 얻습니다. */
+	void GetWindowSize(float& outWidth, float& outHeight);
+
+	/** 커서가 윈도우 내부에 있는지 확인합니다. */
+	bool IsEnterCursor() const { return bIsEnterCursor_; }
+
+	/** 현재 키 값의 입력 상태를 얻습니다. */
+	EPress GetKeyPress(const EKey& key);
+
+	/** 현재 마우스의 입력 상태를 얻습니다. */
+	EPress GetMousePress(const EMouse& mouse);
+
+	/** Tick 호출 이전의 커서 위치를 얻습니다. */
+	const glm::vec2& GetPrevCursorPos() const { return prevCursorPos_; }
+
+	/** Tick 호출 이후의 커서 위치를 얻습니다. */
+	const glm::vec2& GetCurrCursorPos() const { return currCursorPos_; }
+
+	/** GLFW 매니저에 윈도우 이벤트 액션을 등록합니다. */
+	WindowEventID AddWindowEventAction(const EWindowEvent& windowEvent, const std::function<void()>& eventAction, bool bIsActive = true);
+
+	/** GLFW 매니저에 윈도우 이벤트 액션을 삭제합니다. */
+	void DeleteWindowEventAction(const WindowEventID& windowEventID);
+
+	/** 등록된 윈도우 이벤트 액션의 활성화 여부를 설정합니다. */
+	void SetActiveWindowEventAction(const WindowEventID& windowEventID, bool bIsActive);
+
 private:
 	/**
 	 * GLFW 매니저의 기본 생성자와 빈 가상 소멸자입니다.
@@ -192,7 +233,116 @@ private:
 	GLFWManager() = default;
 	virtual ~GLFWManager() {}
 
+	/** 윈도우 이벤트 발생 시 실행할 동작입니다. */
+	struct WindowEventAction
+	{
+		EWindowEvent          windowEvent = EWindowEvent::NONE; /** 윈도우 이벤트의 종류입니다. */
+		std::function<void()> windowEventAction = nullptr;      /** 이벤트 감지 시 실행할 액션입니다. */
+		bool                  bIsActive = false;                /** 이벤트 활성화 여부입니다. */
+
+	};
+
+	/** GLFW 에러 처리를 위한 콜백 함수입니다. */
+	static void ErrorCallback(int32_t errorCode, const char* description);
+
+	/** 마우스 커서가 움직일 때 호출되는 콜백 함수입니다. */
+	static void CursorMoveCallback(GLFWwindow* window, double x, double y);
+
+	/** 마우스 커서가 진입했을 때 호출되는 콜백 함수입니다. */
+	static void CursorEnterCallback(GLFWwindow* window, int32_t entered);
+
+	/** 윈도우 창이 움직였을 때 호출되는 콜백 함수입니다. */
+	static void MoveWindowCallback(GLFWwindow* window, int32_t x, int32_t y);
+
+	/** 윈도우 창의 포커스 관련 요소가 변경되었을 때 콜백 함수입니다. */
+	static void FocusWindowCallback(GLFWwindow* window, int32_t focused);
+
+	/** 윈도우 창을 닫았을 때 호출되는 콜백 함수입니다. */
+	static void CloseWindowCallback(GLFWwindow* window);
+
+	/** 커서의 윈도우 창 진입 여부를 설정합니다. */
+	void SetCursorEnter(int32_t entered);
+
+	/** 현재 커서 위치를 설정합니다. */
+	void SetCursorPosition(double x, double y);
+
+	/** 윈도우 움직임을 설정합니다. */
+	void SetWindowMove(int32_t x, int32_t y);
+
+	/** 윈도우 포커스 여부를 설정합니다. */
+	void SetWindowFocus(int32_t focused);
+
+	/** 윈도우 종료 이벤트를 설정합니다. */
+	void SetWindowClose();
+
+	/** 해당 키가 눌렸는지 확인합니다. */
+	bool IsPressKey(const int32_t* keyboardState, const EKey& key);
+
+	/** 키보드 상태를 업데이트합니다. */
+	void UpdateKeyboardState();
+
+	/** 마우스 버튼이 눌렸는지 확인합니다. */
+	bool IsPressButton(const int32_t* mouseState, const EMouse& mouse);
+
+	/** 마우스 상태를 업데이트합니다. */
+	void UpdateMouseState();
+
+	/** 윈도우 이벤트 액션을 실행합니다. */
+	void RunWindowEventAction(const EWindowEvent& windowEvent);
+
 private:
 	/** GLFW 매니저의 싱글턴 객체입니다. */
 	static GLFWManager singleton_;
+
+	/** GLFW 매니저가 관리하는 메인 윈도우입니다. */
+	GLFWwindow* mainWindow_ = nullptr;
+
+	/** GLFW 매니저가 관리하는 메인 윈도우의 가로/세로 크기입니다. */
+	int32_t mainWindowWidth_ = 0;
+	int32_t mainWindowHeight_ = 0;
+
+	/** 커서가 윈도우 내부에 있는지 확인합니다. */
+	bool bIsEnterCursor_ = true;
+
+	/** Tick 호출 이전의 커서 위치입니다. */
+	glm::vec2 prevCursorPos_ = glm::vec2();
+
+	/** Tick 호출 이후의 커서 위치입니다. */
+	glm::vec2 currCursorPos_ = glm::vec2();
+
+	/** 키 상태 배열의 크기 값입니다. */
+	static const uint32_t KEY_BOARD_STATE_SIZE = 348;
+
+	/** Tick 호출 이전의 키 상태입니다. */
+	std::array<int32_t, KEY_BOARD_STATE_SIZE> prevKeyboardState_;
+
+	/** Tick 호출 이후의 키 상태입니다. */
+	std::array<int32_t, KEY_BOARD_STATE_SIZE> currKeyboardState_;
+
+	/** 마우스 상태 배열의 크기 값입니다. */
+	static const uint32_t MOUSE_STATE_SIZE = 3;
+
+	/** Tick 호출 이전의 마우스 상태입니다. */
+	std::array<int32_t, MOUSE_STATE_SIZE> prevMouseState_;
+
+	/** Tick 호출 이후의 마우스 상태입니다. */
+	std::array<int32_t, MOUSE_STATE_SIZE> currMouseState_;
+
+	/** 윈도우 움직임이 시작 되었는지 확인합니다. */
+	bool bIsStartMoveWindow_ = false;
+
+	/** 윈도우 움직임이 감지되었는지 확인합니다. */
+	bool bIsDetectMoveWindow_ = false;
+
+	/** GLFW 에러 발생 여부입니다. */
+	bool bIsDetectError_ = false;
+
+	/** 최대 등록할 수 있는 윈도우 이벤트 액션 수입니다. */
+	static const uint32_t MAX_EVENT_ACTION_SIZE = 100;
+
+	/** 현재 윈도우 이벤트 액션의 버퍼 크기입니다. */
+	uint32_t windowEventActionSize_ = 0;
+
+	/** 동작할 윈도우 이벤트 액션 목록입니다. */
+	std::array<WindowEventAction, MAX_EVENT_ACTION_SIZE> windowEventActions_;
 };
--- DodgeBall/Src/GLFW/GLFWManager.cpp
+++ DodgeBall/Src/GLFW/GLFWManager.cpp
@@ -1,4 +1,171 @@
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include "GLFW/GLFWAssert.h"
 #include "GLFW/GLFWManager.h"
+
+#include "Utils/Assertion.h"
+#include "Utils/MemoryAlloc.h"
+
+#define GL_MAJOR_VERSION 4
+#define GL_MINOR_VERSION 6
+
+GLFWManager GLFWManager::singleton_;
+
+
+static const std::array<EKey, 119> KEYS =
+{
+	EKey::KEY_SPACE,
+	EKey::KEY_APOSTROPHE,
+	EKey::KEY_COMMA,
+	EKey::KEY_MINUS,
+	EKey::KEY_PERIOD,
+	EKey::KEY_SLASH,
+	EKey::KEY_0,
+	EKey::KEY_1,
+	EKey::KEY_2,
+	EKey::KEY_3,
+	EKey::KEY_4,
+	EKey::KEY_5,
+	EKey::KEY_6,
+	EKey::KEY_7,
+	EKey::KEY_8,
+	EKey::KEY_9,
+	EKey::KEY_SEMICOLON,
+	EKey::KEY_EQUAL,
+	EKey::KEY_A,
+	EKey::KEY_B,
+	EKey::KEY_C,
+	EKey::KEY_D,
+	EKey::KEY_E,
+	EKey::KEY_F,
+	EKey::KEY_G,
+	EKey::KEY_H,
+	EKey::KEY_I,
+	EKey::KEY_J,
+	EKey::KEY_K,
+	EKey::KEY_L,
+	EKey::KEY_M,
+	EKey::KEY_N,
+	EKey::KEY_O,
+	EKey::KEY_P,
+	EKey::KEY_Q,
+	EKey::KEY_R,
+	EKey::KEY_S,
+	EKey::KEY_T,
+	EKey::KEY_U,
+	EKey::KEY_V,
+	EKey::KEY_W,
+	EKey::KEY_X,
+	EKey::KEY_Y,
+	EKey::KEY_Z,
+	EKey::KEY_LEFT_BRACKET,
+	EKey::KEY_BACKSLASH,
+	EKey::KEY_RIGHT_BRACKET,
+	EKey::KEY_GRAVE_ACCENT,
+	EKey::KEY_WORLD_1,
+	EKey::KEY_WORLD_2,
+	EKey::KEY_ESCAPE,
+	EKey::KEY_ENTER,
+	EKey::KEY_TAB,
+	EKey::KEY_BACKSPACE,
+	EKey::KEY_INSERT,
+	EKey::KEY_DELETE,
+	EKey::KEY_RIGHT,
+	EKey::KEY_LEFT,
+	EKey::KEY_DOWN,
+	EKey::KEY_UP,
+	EKey::KEY_PAGE_UP,
+	EKey::KEY_PAGE_DOWN,
+	EKey::KEY_HOME,
+	EKey::KEY_END,
+	EKey::KEY_CAPS_LOCK,
+	EKey::KEY_SCROLL_LOCK,
+	EKey::KEY_NUM_LOCK,
+	EKey::KEY_PRINT_SCREEN,
+	EKey::KEY_PAUSE,
+	EKey::KEY_F1,
+	EKey::KEY_F2,
+	EKey::KEY_F3,
+	EKey::KEY_F4,
+	EKey::KEY_F5,
+	EKey::KEY_F6,
+	EKey::KEY_F7,
+	EKey::KEY_F8,
+	EKey::KEY_F9,
+	EKey::KEY_F10,
+	EKey::KEY_F11,
+	EKey::KEY_F12,
+	EKey::KEY_F13,
+	EKey::KEY_F14,
+	EKey::KEY_F15,
+	EKey::KEY_F16,
+	EKey::KEY_F17,
+	EKey::KEY_F18,
+	EKey::KEY_F19,
+	EKey::KEY_F20,
+	EKey::KEY_F21,
+	EKey::KEY_F22,
+	EKey::KEY_F23,
+	EKey::KEY_F24,
+	EKey::KEY_F25,
+	EKey::KEY_KP_0,
+	EKey::KEY_KP_1,
+	EKey::KEY_KP_2,
+	EKey::KEY_KP_3,
+	EKey::KEY_KP_4,
+	EKey::KEY_KP_5,
+	EKey::KEY_KP_6,
+	EKey::KEY_KP_7,
+	EKey::KEY_KP_8,
+	EKey::KEY_KP_9,
+	EKey::KEY_KP_DECIMAL,
+	EKey::KEY_KP_DIVIDE,
+	EKey::KEY_KP_MULTIPLY,
+	EKey::KEY_KP_SUBTRACT,
+	EKey::KEY_KP_ADD,
+	EKey::KEY_KP_ENTER,
+	EKey::KEY_KP_EQUAL,
+	EKey::KEY_LEFT_SHIFT,
+	EKey::KEY_LEFT_CONTROL,
+	EKey::KEY_LEFT_ALT,
+	EKey::KEY_LEFT_SUPER,
+	EKey::KEY_RIGHT_SHIFT,
+	EKey::KEY_RIGHT_CONTROL,
+	EKey::KEY_RIGHT_ALT,
+	EKey::KEY_RIGHT_SUPER,
+};
+
+void GLFWManager::ErrorCallback(int32_t errorCode, const char* description)
+{
+	GLFWError::SetLastError(errorCode, description);
+}
+
+void GLFWManager::CursorMoveCallback(GLFWwindow* window, double x, double y)
+{
+	singleton_.SetCursorPosition(x, y);
+}
+
+void GLFWManager::CursorEnterCallback(GLFWwindow* window, int32_t entered)
+{
+	singleton_.SetCursorEnter(entered);
+}
+
+void GLFWManager::MoveWindowCallback(GLFWwindow* window, int32_t x, int32_t y)
+{
+	singleton_.SetWindowMove(x, y);
+}
+
+void GLFWManager::FocusWindowCallback(GLFWwindow* window, int32_t focused)
+{
+	singleton_.SetWindowFocus(focused);
+}
+
+void GLFWManager::CloseWindowCallback(GLFWwindow* window)
+{
+	singleton_.SetWindowClose();
+}
 
 GLFWManager& GLFWManager::GetRef()
 {
@@ -8,4 +175,310 @@ GLFWManager& GLFWManager::GetRef()
 GLFWManager* GLFWManager::GetPtr()
 {
 	return &singleton_;
+}
+
+
+void GLFWManager::Startup(int32_t width, int32_t height, const char* title, bool bIsWindowCentered)
+{
+	glfwSetErrorCallback(ErrorCallback);
+
+	GLFWallocator allocator;
+	allocator.allocate = MemoryAlloc;
+	allocator.reallocate = MemoryRealloc;
+	allocator.deallocate = MemoryFree;
+
+	GLFW_API_CHECK(glfwInitAllocator(&allocator));
+	GLFW_EXP_CHECK(glfwInit());
+
+	GLFW_API_CHECK(glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_MAJOR_VERSION));
+	GLFW_API_CHECK(glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_MINOR_VERSION));
+	GLFW_API_CHECK(glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE));
+	GLFW_API_CHECK(glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE));
+
+	mainWindowWidth_ = width;
+	mainWindowHeight_ = height;
+
+	mainWindow_ = glfwCreateWindow(width, height, title, nullptr, nullptr);
+	GLFW_EXP_CHECK(mainWindow_ != nullptr);
+
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.IniFilename = nullptr;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableSetMousePos;
+
+	glfwSetCursorPosCallback(mainWindow_, GLFWManager::CursorMoveCallback);
+	glfwSetCursorEnterCallback(mainWindow_, GLFWManager::CursorEnterCallback);
+	glfwSetWindowPosCallback(mainWindow_, GLFWManager::MoveWindowCallback);
+	glfwSetWindowFocusCallback(mainWindow_, GLFWManager::FocusWindowCallback);
+	glfwSetWindowCloseCallback(mainWindow_, GLFWManager::CloseWindowCallback);
+
+	double x = 0.0;
+	double y = 0.0;
+	glfwGetCursorPos(mainWindow_, &x, &y);
+
+	float cursorX = -static_cast<float>(mainWindowWidth_) * 0.5f + static_cast<float>(x);
+	float cursorY = +static_cast<float>(mainWindowHeight_) * 0.5f - static_cast<float>(y);
+	currCursorPos_ = glm::vec2(cursorX, cursorY);
+	prevCursorPos_ = currCursorPos_;
+
+	std::fill(prevKeyboardState_.begin(), prevKeyboardState_.end(), 0);
+	std::fill(currKeyboardState_.begin(), currKeyboardState_.end(), 0);
+	std::fill(prevMouseState_.begin(), prevMouseState_.end(), 0);
+	std::fill(currMouseState_.begin(), currMouseState_.end(), 0);
+
+	ASSERT(ImGui_ImplGlfw_InitForOpenGL(mainWindow_, true), "Failed to initialize ImGui for GLFW");
+
+	if (bIsWindowCentered)
+	{
+		GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+		const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+		int32_t monitorX = 0;
+		int32_t monitorY = 0;
+		glfwGetMonitorPos(monitor, &monitorX, &monitorY);
+
+		int32_t centerX = monitorX + (mode->width - mainWindowWidth_) / 2;
+		int32_t centerY = monitorY + (mode->height - mainWindowHeight_) / 2;
+		glfwSetWindowPos(mainWindow_, centerX, centerY);
+	}
+}
+
+void GLFWManager::Shutdown()
+{
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	if (mainWindow_)
+	{
+		glfwDestroyWindow(mainWindow_);
+		mainWindow_ = nullptr;
+	}
+
+	glfwTerminate();
+}
+
+void GLFWManager::Tick()
+{
+	bIsDetectMoveWindow_ = false;
+	prevCursorPos_ = currCursorPos_;
+
+	glfwPollEvents();
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+
+	UpdateKeyboardState();
+	UpdateMouseState();
+
+	if (bIsStartMoveWindow_ && !bIsDetectMoveWindow_)
+	{
+		bIsStartMoveWindow_ = false;
+		RunWindowEventAction(EWindowEvent::MOVE_LEAVE);
+	}
+}
+
+void GLFWManager::GetWindowSize(float& outWidth, float& outHeight)
+{
+	outWidth = static_cast<float>(mainWindowWidth_);
+	outHeight = static_cast<float>(mainWindowHeight_);
+}
+
+EPress GLFWManager::GetKeyPress(const EKey& key)
+{
+	EPress press = EPress::NONE;
+
+	if (IsPressKey(prevKeyboardState_.data(), key))
+	{
+		if (IsPressKey(currKeyboardState_.data(), key))
+		{
+			press = EPress::HELD;
+		}
+		else
+		{
+			press = EPress::RELEASED;
+		}
+	}
+	else
+	{
+		if (IsPressKey(currKeyboardState_.data(), key))
+		{
+			press = EPress::PRESSED;
+		}
+		else
+		{
+			press = EPress::NONE;
+		}
+	}
+
+	return press;
+}
+
+EPress GLFWManager::GetMousePress(const EMouse& mouse)
+{
+	EPress press = EPress::NONE;
+
+	if (IsPressButton(prevMouseState_.data(), mouse))
+	{
+		if (IsPressButton(currMouseState_.data(), mouse))
+		{
+			press = EPress::HELD;
+		}
+		else
+		{
+			press = EPress::RELEASED;
+		}
+	}
+	else
+	{
+		if (IsPressButton(currMouseState_.data(), mouse))
+		{
+			press = EPress::PRESSED;
+		}
+		else
+		{
+			press = EPress::NONE;
+		}
+	}
+
+	return press;
+}
+
+WindowEventID GLFWManager::AddWindowEventAction(const EWindowEvent& windowEvent, const std::function<void()>& eventAction, bool bIsActive)
+{
+	CHECK(0 <= windowEventActionSize_ && windowEventActionSize_ < MAX_EVENT_ACTION_SIZE);
+
+	WindowEventID windowEventID = -1;
+	for (int32_t index = 0; index < windowEventActionSize_; ++index)
+	{
+		if (windowEventActions_[index].windowEvent == EWindowEvent::NONE)
+		{
+			windowEventID = static_cast<WindowEventID>(index);
+			break;
+		}
+	}
+
+	if (windowEventID == -1)
+	{
+		windowEventID = windowEventActionSize_++;
+	}
+
+	windowEventActions_[windowEventID].windowEvent = windowEvent;
+	windowEventActions_[windowEventID].windowEventAction = eventAction;
+	windowEventActions_[windowEventID].bIsActive = bIsActive;
+
+	return windowEventID;
+}
+
+void GLFWManager::DeleteWindowEventAction(const WindowEventID& windowEventID)
+{
+	CHECK(0 <= windowEventID && windowEventID < MAX_EVENT_ACTION_SIZE);
+
+	windowEventActions_[windowEventID].windowEvent = EWindowEvent::NONE;
+	windowEventActions_[windowEventID].windowEventAction = nullptr;
+	windowEventActions_[windowEventID].bIsActive = false;
+}
+
+void GLFWManager::SetActiveWindowEventAction(const WindowEventID& windowEventID, bool bIsActive)
+{
+	CHECK(0 <= windowEventID && windowEventID < MAX_EVENT_ACTION_SIZE);
+	windowEventActions_[windowEventID].bIsActive = bIsActive;
+}
+
+void GLFWManager::SetCursorEnter(int32_t entered)
+{
+	bIsEnterCursor_ = static_cast<bool>(entered);
+}
+
+void GLFWManager::SetCursorPosition(double x, double y)
+{
+	float cursorX = -static_cast<float>(mainWindowWidth_) * 0.5f + static_cast<float>(x);
+	float cursorY = +static_cast<float>(mainWindowHeight_) * 0.5f - static_cast<float>(y);
+	currCursorPos_ = glm::vec2(cursorX, cursorY);
+}
+
+void GLFWManager::SetWindowMove(int32_t x, int32_t y)
+{
+	if (bIsStartMoveWindow_)
+	{
+		bIsDetectMoveWindow_ = true;
+		return;
+	}
+
+	bIsStartMoveWindow_ = true;
+	RunWindowEventAction(EWindowEvent::MOVE_ENTER);
+}
+
+void GLFWManager::SetWindowFocus(int32_t focused)
+{
+	EWindowEvent windowEvent = EWindowEvent::NONE;
+	if (focused)
+	{
+		windowEvent = EWindowEvent::FOCUS_GAIN;
+	}
+	else
+	{
+		windowEvent = EWindowEvent::FOCUS_LOST;
+	}
+
+	RunWindowEventAction(windowEvent);
+}
+
+void GLFWManager::SetWindowClose()
+{
+	RunWindowEventAction(EWindowEvent::CLOSE_WINDOW);
+}
+
+bool GLFWManager::IsPressKey(const int32_t* keyboardState, const EKey& key)
+{
+	return keyboardState[static_cast<int32_t>(key)];
+}
+
+void GLFWManager::UpdateKeyboardState()
+{
+	std::copy(currKeyboardState_.begin(), currKeyboardState_.end(), prevKeyboardState_.begin());
+	std::fill(currKeyboardState_.begin(), currKeyboardState_.end(), 0);
+
+	for (const auto& KEY : KEYS)
+	{
+		int32_t key = static_cast<int32_t>(KEY);
+		currKeyboardState_[key] = glfwGetKey(mainWindow_, key);
+	}
+}
+
+bool GLFWManager::IsPressButton(const int32_t* mouseState, const EMouse& mouse)
+{
+	return mouseState[static_cast<int32_t>(mouse)];
+}
+
+void GLFWManager::UpdateMouseState()
+{
+	std::copy(currMouseState_.begin(), currMouseState_.end(), prevMouseState_.begin());
+	std::fill(currMouseState_.begin(), currMouseState_.end(), 0);
+
+	static const std::array<EMouse, MOUSE_STATE_SIZE> MOUSE_CODES =
+	{
+		EMouse::LEFT,
+		EMouse::RIGHT,
+		EMouse::MIDDLE,
+	};
+
+	for (const auto& MOUSE_CODE : MOUSE_CODES)
+	{
+		int32_t mouse = static_cast<int32_t>(MOUSE_CODE);
+		currMouseState_[mouse] = glfwGetMouseButton(mainWindow_, mouse);
+	}
+}
+
+void GLFWManager::RunWindowEventAction(const EWindowEvent& windowEvent)
+{
+	for (uint32_t index = 0; index < windowEventActionSize_; ++index)
+	{
+		WindowEventAction& windowEventAction = windowEventActions_[index];
+
+		if (windowEventAction.windowEvent == windowEvent && windowEventAction.bIsActive && windowEventAction.windowEventAction)
+		{
+			windowEventAction.windowEventAction();
+		}
+	}
 }
