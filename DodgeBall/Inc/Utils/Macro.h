cb1be184861386a1ce0417d12861ea7c93c2cbff
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-14 07:42:15 +0900

    타입의 복사 생성자와 대입 연산자를 명시적으로 삭제하는 매크로 추가

---
+++ DodgeBall/Inc/Utils/Macro.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * 타입의 복사 생성자와 대입 연산자를 명시적으로 삭제하는 매크로입니다.
+ * 이 매크로가 정의 되어 있지 않은 타입은 반드시 명시적으로 복사 생성자와 대입 연산자를 정의해야 합니다.
+ */
+#ifndef DISALLOW_COPY_AND_ASSIGN
+#define DISALLOW_COPY_AND_ASSIGN(TypeName)\
+TypeName(TypeName&&) = delete;\
+TypeName(const TypeName&) = delete;\
+TypeName& operator=(TypeName&&) = delete;\
+TypeName& operator=(const TypeName&) = delete;
+#endif
