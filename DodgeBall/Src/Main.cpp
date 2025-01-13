de23b3795f0090f94ac23dc51df129374211fd1f
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-14 07:33:44 +0900

    기본적인 GLFW 초기화 및 아무 동작도 수행하지 않는 루프 추가

--- DodgeBall/Src/Main.cpp
+++ DodgeBall/Src/Main.cpp
@@ -5,7 +5,38 @@
 #include <crtdbg.h>
 #endif
 
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(1000, 800, "DodgeBall", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, GLFW_TRUE);
+		}
+
+		glClearColor(0.1f, 0.1f, 0.7f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
 	return 0;
 }
