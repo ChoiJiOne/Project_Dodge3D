ad4258f8364ef6f3fc6d84f5bc08e366b2512d8d
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-14 10:23:23 +0900

    이전 프로젝트에서 사용하던 GL 매니저를 현재 프로젝트에 맞게 변경 후 추가

--- DodgeBall/Inc/GL/GLManager.h
+++ DodgeBall/Inc/GL/GLManager.h
@@ -6,6 +6,10 @@
 #include <string>
 #include <vector>
 
+#include <glfw/glfw3.h>
+
+#include "GL/GLResource.h"
+
 #include "Utils/Macro.h"
 
 /**
@@ -23,6 +27,82 @@ public:
 	/** GL 매니저의 싱글턴 객체 포인터를 얻습니다. */
 	static GLManager* GetPtr();
 
+	/** GL 매니저를 초기화합니다. 이때, GL 매니저를 초기화 하기 전에 반드시 GLFW 매니저를 초기화 해야 합니다. */
+	void Startup();
+
+	/** GL 매니저의 초기화를 해제합니다. */
+	void Shutdown();
+
+	/** 프레임 렌더링을 시작합니다. */
+	void BeginFrame(float red, float green, float blue, float alpha, float depth = 1.0f, uint8_t stencil = 0);
+
+	/** 프레임 렌더링을 종료합니다. */
+	void EndFrame();
+
+	/** Viewport를 설정합니다. */
+	void SetViewport(int32_t x, int32_t y, int32_t width, int32_t height);
+
+	/** OpenGL의 전역 상태 머신 설정을 수행합니다. */
+	void SetVsyncMode(bool bIsEnable);
+	void SetDepthMode(bool bIsEnable);
+	void SetStencilMode(bool bIsEnable);
+	void SetAlphaBlendMode(bool bIsEnable);
+	void SetCullFaceMode(bool bIsEnable);
+
+	/** OpenGL 리소스를 생성합니다. */
+	template <typename TResource, typename... Args>
+	TResource* Create(Args&&... args)
+	{
+		int32_t resourceID = -1;
+		for (uint32_t index = 0; index < resources_.size(); ++index)
+		{
+			if (!resources_[index].first && !resources_[index].second)
+			{
+				resourceID = static_cast<int32_t>(index);
+				break;
+			}
+		}
+
+		if (resourceID == -1)
+		{
+			resourceID = resources_.size();
+			resources_.push_back({ nullptr, true });
+			resources_[resourceID].first = std::make_unique<TResource>(args...);
+		}
+		else
+		{
+			resources_[resourceID].second = true;
+			resources_[resourceID].first = std::make_unique<TResource>(args...);
+		}
+
+		return reinterpret_cast<TResource*>(resources_[resourceID].first.get());
+	}
+
+	/** 생성한 OpenGL 리소스를 파괴합니다. */
+	void Destroy(const GLResource* resource);
+
+	/** 리소스를 GL 매니저에 등록합니다. */
+	void Register(const std::string& name, GLResource* resource);
+
+	/** 리소스 이름이 등록 되었는지 확인합니다. */
+	bool IsRegistration(const std::string& name);
+
+	/** GL 매니저에 등록을 해제합니다. */
+	void Unregister(const std::string& name);
+
+	/** 이름에 대응하는 리소스를 얻습니다. */
+	template <typename TResource>
+	TResource* GetByName(const std::string& name)
+	{
+		auto it = namedResources_.find(name);
+		if (it == namedResources_.end())
+		{
+			return nullptr;
+		}
+
+		return reinterpret_cast<TResource*>(it->second);
+	}
+
 private:
 	/**
 	 * GL 매니저의 기본 생성자와 빈 가상 소멸자입니다.
@@ -34,4 +114,17 @@ private:
 private:
 	/** GL 매니저의 싱글턴 객체입니다. */
 	static GLManager singleton_;
+
+	/** 렌더링 대상이 되는 윈도우입니다. */
+	GLFWwindow* renderTargetWindow_ = nullptr;
+
+	/** 렌더링 대상이 되는 윈도우의 가로/세로 크기입니다. */
+	int32_t windowWidth_ = 0;
+	int32_t windowHeight_ = 0;
+
+	/** OpenGL 리소스의 버퍼와 해당 버퍼의 현재 사용여부입니다. */
+	std::vector<std::pair<std::unique_ptr<GLResource>, bool>> resources_;
+
+	/** 이름을 가진 리소스입니다. */
+	std::map<std::string, GLResource*> namedResources_;
 };
--- DodgeBall/Src/GL/GLManager.cpp
+++ DodgeBall/Src/GL/GLManager.cpp
@@ -1,13 +1,14 @@
 #include <glad/glad.h>
-#include <glfw/glfw3.h>
 
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+#include "GL/GLAssert.h"
 #include "GL/GLManager.h"
 
 #include "GLFW/GLFWAssert.h"
+#include "GLFW/GLFWManager.h"
 
 #include "Utils/Assertion.h"
 
@@ -21,4 +22,167 @@ GLManager& GLManager::GetRef()
 GLManager* GLManager::GetPtr()
 {
 	return &singleton_;
+}
+
+void GLManager::Startup()
+{
+	GLFWManager& glfwManager = GLFWManager::GetRef();
+	renderTargetWindow_ = glfwManager.mainWindow_;
+	windowWidth_ = glfwManager.mainWindowWidth_;
+	windowHeight_ = glfwManager.mainWindowHeight_;
+
+	GLFW_API_CHECK(glfwMakeContextCurrent(renderTargetWindow_));
+
+	ASSERT(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress), "Failed to initialize OpenGL function.");
+	ASSERT(ImGui_ImplOpenGL3_Init(), "Failed to initialize ImGui for OpenGL.");
+}
+
+void GLManager::Shutdown()
+{
+	ImGui_ImplOpenGL3_Shutdown();
+
+	for (uint32_t index = 0; index < resources_.size(); ++index)
+	{
+		if (resources_[index].first)
+		{
+			if (resources_[index].first->IsInitialized())
+			{
+				resources_[index].first->Release();
+			}
+
+			resources_[index].first.reset();
+			resources_[index].second = false;
+		}
+	}
+
+	renderTargetWindow_ = nullptr;
+}
+
+void GLManager::BeginFrame(float red, float green, float blue, float alpha, float depth, uint8_t stencil)
+{
+	SetViewport(0, 0, windowWidth_, windowHeight_);
+
+	glClearColor(red, green, blue, alpha);
+	glClearDepth(depth);
+	glClearStencil(stencil);
+
+	GL_API_CHECK(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT));
+}
+
+void GLManager::EndFrame()
+{
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+	GLFW_API_CHECK(glfwSwapBuffers(renderTargetWindow_));
+}
+
+void GLManager::SetViewport(int32_t x, int32_t y, int32_t width, int32_t height)
+{
+	GL_API_CHECK(glViewport(x, y, width, height));
+}
+
+void GLManager::SetVsyncMode(bool bIsEnable)
+{
+	GLFW_API_CHECK(glfwSwapInterval(static_cast<int32_t>(bIsEnable)));
+}
+
+void GLManager::SetDepthMode(bool bIsEnable)
+{
+	if (bIsEnable)
+	{
+		GL_API_CHECK(glEnable(GL_DEPTH_TEST));
+	}
+	else
+	{
+		GL_API_CHECK(glDisable(GL_DEPTH_TEST));
+	}
+}
+
+void GLManager::SetStencilMode(bool bIsEnable)
+{
+	if (bIsEnable)
+	{
+		GL_API_CHECK(glEnable(GL_STENCIL_TEST));
+	}
+	else
+	{
+		GL_API_CHECK(glDisable(GL_STENCIL_TEST));
+	}
+}
+
+void GLManager::SetAlphaBlendMode(bool bIsEnable)
+{
+	if (bIsEnable)
+	{
+		GL_API_CHECK(glEnable(GL_BLEND));
+		GL_API_CHECK(glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ZERO));
+	}
+	else
+	{
+		GL_API_CHECK(glDisable(GL_BLEND));
+	}
+}
+
+void GLManager::SetCullFaceMode(bool bIsEnable)
+{
+	if (bIsEnable)
+	{
+		GL_API_CHECK(glEnable(GL_CULL_FACE));
+	}
+	else
+	{
+		GL_API_CHECK(glDisable(GL_CULL_FACE));
+	}
+}
+
+void GLManager::Destroy(const GLResource* resource)
+{
+	int32_t resourceID = -1;
+	for (uint32_t index = 0; index < resources_.size(); ++index)
+	{
+		GLResource* resourcePtr = resources_[index].first.get();
+		if (resource == resourcePtr)
+		{
+			resourceID = static_cast<int32_t>(index);
+			break;
+		}
+	}
+
+	if (resourceID == -1)
+	{
+		return; // 해당 리소스는 이미 할당 해제 되었거나, GLManager에 의해서 생성된 리소스가 아님.
+	}
+
+	if (resources_[resourceID].first)
+	{
+		if (resources_[resourceID].first->IsInitialized())
+		{
+			resources_[resourceID].first->Release();
+		}
+
+		resources_[resourceID].first.reset();
+		resources_[resourceID].second = false;
+	}
+}
+
+void GLManager::Register(const std::string& name, GLResource* resource)
+{
+	auto it = namedResources_.find(name);
+	ASSERT(it == namedResources_.end(), "Already register '%s'", name.c_str());
+
+	namedResources_.insert({ name, resource });
+}
+
+bool GLManager::IsRegistration(const std::string& name)
+{
+	return namedResources_.find(name) != namedResources_.end();
+}
+
+void GLManager::Unregister(const std::string& name)
+{
+	auto it = namedResources_.find(name);
+	ASSERT(it != namedResources_.end(), "Can't find '%s' in GLManager.", name.c_str());
+
+	namedResources_.erase(it);
 }
