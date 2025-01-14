d2b31c0dc8c8bb880d6173f8ea31265da6f5a103
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-14 11:48:49 +0900

    아무 동작도 수행하지 않는 루프 추가

--- DodgeBall/Src/Main.cpp
+++ DodgeBall/Src/Main.cpp
@@ -6,37 +6,27 @@
 #endif
 
 #include <glad/glad.h>
-#include <glfw/glfw3.h>
+
+#include "GL/GLManager.h"
+#include "GLFW/GLFWManager.h"
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWManager::GetRef().Startup(1000, 800, "DodgeBall", true);
+	GLManager::GetRef().Startup();
 
-	GLFWwindow* window = glfwCreateWindow(1000, 800, "DodgeBall", nullptr, nullptr);
-	glfwMakeContextCurrent(window);
+	bool bIsDone = false;
+	GLFWManager::GetRef().AddWindowEventAction(EWindowEvent::CLOSE_WINDOW, [&]() { bIsDone = true; }, true);
 
-	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-
-	while (!glfwWindowShouldClose(window))
+	while (!bIsDone)
 	{
-		glfwPollEvents();
-		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		{
-			glfwSetWindowShouldClose(window, GLFW_TRUE);
-		}
+		GLFWManager::GetRef().Tick();
 
-		glClearColor(0.1f, 0.1f, 0.7f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(window);
+		GLManager::GetRef().BeginFrame(1.0f, 0.0f, 0.0f, 1.0f);
+		GLManager::GetRef().EndFrame();
 	}
-
-	glfwDestroyWindow(window);
-
-	glfwTerminate();
+	
+	GLManager::GetRef().Shutdown();
+	GLFWManager::GetRef().Shutdown();
 	return 0;
 }
